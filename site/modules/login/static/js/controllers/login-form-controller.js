6e730f6b5cffe171c4dee46360c84738c10179ca
Jaan Oras <jaan.oras@guardtime.com>
2013-02-25 16:36:37 -0800

    Style cleanup, minor UI changes.

--- site/modules/login/static/js/controllers/login-form-controller.js
+++ site/modules/login/static/js/controllers/login-form-controller.js
@@ -1,22 +1,21 @@
 'use strict';
 
 (function (ng, app) {
-	app.controller(
-		'LoginFormController',
-		function ($scope, $http, Login, $window) {
-			$scope.login = {
-				email:'',
-				password:'',
-				remember:''
-			};
+    app.controller(
+        'LoginFormController',
+        function ($scope, $http, Login, $window) {
+            $scope.login = {
+                email:'',
+                password:'',
+                remember:''
+            };
 
-			$scope.logIn = function () {
-				Login($scope.login, function (result) {
-					if (result.success) {
-						$window.location.href = "/app#!/machine";
-					}
-				});
-			};
-		}
-	);
+            $scope.logIn = function () {
+                Login($scope.login, function (result) {
+                    if (result.success) {
+                        $window.location.href = '/app#!/machine';
+                    }
+                });
+            };
+        });
 })(window.angular, window.LoginModule);
