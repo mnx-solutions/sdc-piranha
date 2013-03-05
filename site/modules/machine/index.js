f3df1fe688f49524a6ba28708b93c96769f7894c
endrikend <endrik.end@gmail.com>
2013-03-05 15:14:26 -0800

    missing semicolon

--- site/modules/machine/index.js
+++ site/modules/machine/index.js
@@ -4,7 +4,6 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-//    res.json(require('./static/machines.json'));
 	req.cloud.listMachines(function (err, machines) {
 	if (!err) {
 			res.json(machines);
