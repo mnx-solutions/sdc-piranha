0c7903db886e4cc73a0506f5160ff12d33d70e26
Jaan Oras <jaan.oras@guardtime.com>
2013-03-11 00:44:15 -0700

    Created unified machine service that uses the new job layer of serverside calls.

--- site/modules/machine/static/js/services/machine.js
+++ site/modules/machine/static/js/services/machine.js
@@ -2,95 +2,118 @@
 
 
 (function (app) {
-    app.factory('Machines', ['$resource',"serverCall", function ($resource, serverCall) {
+    app.factory('Machines', ['$resource', "serverCall", "$rootScope", "$q", function ($resource, serverCall, $rootScope, $q) {
         var service = {};
 
-        var machines = [];
+        var machineList = {job: null, machines: []};
+
+        var findMachine = function (id) {
+            var filtered = machineList.machines.filter(function (machine) {
+                return machine.id === id;
+            });
+
+            if (filtered.length == 1) {
+                return filtered[0];
+            }
+            return null;
+        }
 
         // load machines
-        service.updateMachines = function () {
-            serverCall("MachineList", null, function (err, machineList) {
+        service.updateMachines = function (callback) {
+
+            if (machineList.job && !machineList.job.finished) {
+                machineList.job.addCallback(callback);
+                return machineList.job;
+            }
+
+            machineList.job = serverCall("MachineList", null, function (err, result) {
                 if (!err) {
-                    machines.length = 0;
-                    machines.push.apply(machines, machineList);
+                    machineList.machines.length = 0;
+                    machineList.machines.push.apply(machineList.machines, result);
                 }
             }, function (data) {
-                console.log('progress: ' + data);
+                // handle progress
             });
+
+            machineList.job.addCallback(callback);
         };
 
+        // run updateMachines
         service.updateMachines();
 
         // get reference to the machines list
         service.getMachines = function () {
-            return machines;
+            return machineList;
         };
 
         // find machine by uuid
         service.getMachine = function (uuid) {
-            return machines.filter(function (machine) {
-                return machine.id === uuid;
+
+            var machine = findMachine(uuid);
+
+            // got the machine, return machine
+            if (machine) {
+                return machine;
+            }
+
+            // get the new machine list.
+            var job = service.updateMachines();
+
+            var deferred = $q.defer();
+            job.addCallback(function () {
+                var machine = findMachine(uuid);
+                deferred.resolve(machine);
             });
+
+            return deferred.promise;
         };
 
-        return service;
-    }]);
-
-    app.factory('MachineInfo', ['$resource', '$timeout', '$q',"serverCall", function ($resource, $timeout, $q, serverCall) {
-        var service = {};
-
-        service.getMachine = function (uuid) {
-            var deferred = $q.defer();
-            var machine = null;
-
-            serverCall("MachineDetails", uuid, function (err, result) {
-                if (!err){
-                    deferred.resolve(result);
-                }
-            });
-
-            return deferred.promise;
-        }
-
         service.startMachine = function (uuid) {
-            var deferred = $q.defer();
-            var machine = null;
+            //XXX check for running jobs
+            var machine = findMachine(uuid);
 
-            serverCall("MachineStart", uuid, function (err, result) {
-                if (!err){
-                    deferred.resolve(result);
+            machine.job = serverCall("MachineStart", uuid, function (err, result) {
+                if (!err) {
+                    machine.state = result.state;
                 }
+            }, function (progress) {
+                machine.state = progress.state;
             });
 
-            return deferred.promise;
+            return machine.job;
         }
+
         service.stopMachine = function (uuid) {
-            var deferred = $q.defer();
-            var machine = null;
+            //XXX check for running jobs
+            var machine = findMachine(uuid);
 
-            serverCall("MachineStop", uuid, function (err, result) {
-                if (!err){
-                    deferred.resolve(result);
+            machine.job = serverCall("MachineStop", uuid, function (err, result) {
+                if (!err) {
+                    machine.state = result.state;
                 }
+            }, function (progress) {
+                machine.state = progress.state;
             });
 
-            return deferred.promise;
+            return machine.job;
         }
+
 
         service.rebootMachine = function (uuid) {
-            var deferred = $q.defer();
-            var machine = null;
+            var machine = findMachine(uuid);
 
-            serverCall("MachineReboot", uuid, function (err, result) {
-                if (!err){
-                    deferred.resolve(result);
+            machine.job = serverCall("MachineReboot", uuid, function (err, result) {
+                if (!err) {
+                    machine.state = result.state;
                 }
+            }, function (progress) {
+                machine.state = progress.state;
             });
 
-            return deferred.promise;
+            return machine.job;
         }
+
 
         return service;
     }]);
-
 }(window.JP.getModule('Machine')));
