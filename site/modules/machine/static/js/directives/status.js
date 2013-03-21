f56ea54611f22b2222f01634bee71b44249bb310
Jaan Oras <jaan.oras@guardtime.com>
2013-03-21 15:56:31 -0700

    Cleaned up and redesigned machine details page.

--- site/modules/machine/static/js/directives/status.js
+++ site/modules/machine/static/js/directives/status.js
@@ -24,7 +24,7 @@
             template: '<span class="label" ng-class="labelForState(machine.state)" ng-show="!machine.job.running && !machine.job.failed">' +
                 '{{machine.state}}</span>' +
                 '<span class="label label-info" ng-show="machine.job.running">' +
-                '<img src="static/img/ajax-loader.gif"/>{{machine.job.name}}:{{machine.state}}</span>' +
+                '<img src="static/img/ajax-loader.gif"/>{{machine.job.name}}</span>' +
                 '<span class="label label-inverse" ng-show="!machine.job.running && machine.job.failed"><i class="icon-warning-sign icon-white"/>' +
                 '</span>'
         };
