d8e66cf0e2d190b3b3c2029e5b7546fdca930563
endrikend <endrik.end@gmail.com>
2013-04-30 13:44:33 +0300

    signup step containers

--- site/modules/signup/static/js/controllers/signup-layout.js
+++ site/modules/signup/static/js/controllers/signup-layout.js
@@ -4,7 +4,9 @@
     app.controller(
         'signup.LayoutController',
         ['$scope', 'requestContext', function ($scope, requestContext) {
+                console.log('signup layout');
                 requestContext.setUpRenderContext('signup', $scope);
+
             }
         ]);
 }(window.JP.getModule('Signup')));
