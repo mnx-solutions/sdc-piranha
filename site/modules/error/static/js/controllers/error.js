13dd63ff5f87d832dc119ecb3004ab241f44155c
Harri Siirak <harri.siirak@gmail.com>
2013-05-02 14:37:01 +0300

    Better design for inline error page

--- site/modules/error/static/js/controllers/error.js
+++ site/modules/error/static/js/controllers/error.js
@@ -1,13 +1,18 @@
 'use strict';
 
 (function (ng, app) {
-    console.log('LISTEN LISTEN!')
     app.controller(
         'error.IndexController',
         [ '$scope', '$rootScope', '$route', '$routeParams', 'requestContext', 'errorContext',
 
             function ($scope, $rootScope, $route, $routeParams, requestContext, errorContext) {
                 requestContext.setUpRenderContext('error.index', $scope);
+
+                $scope.$on('errorContextChanged', function (scope, context) {
+                    $scope.error = context.getContext().err;
+                });
+
+                $scope.error = errorContext.getContext().err;
 
             }
         ]);
