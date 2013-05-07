30dc20cdf533416428e3507790d4076a1f779601
thomas m. vaht <zammalhabe@mac.com>
2013-05-07 14:53:07 +0300

    dashboard fixes

--- site/modules/dashboard/static/js/filters/date.js
+++ site/modules/dashboard/static/js/filters/date.js
@@ -2,6 +2,6 @@
 
 window.JP.main.filter('date', function () {
     return function (dateString) {
-        return window.moment(new Date(dateString)).format("L");
+        return window.moment(new Date(dateString)).format("MMM Do");
     };
 });
--- site/modules/dashboard/static/vendor/rss.js
+++ site/modules/dashboard/static/vendor/rss.js
@@ -1,6 +1,6 @@
 function parseRSS(url, callback) {
     $.ajax({
-        url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+        url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=7&callback=?&q=' + encodeURIComponent(url),
         dataType: 'json',
         success: function(data) {
             callback(data.responseData.feed);
