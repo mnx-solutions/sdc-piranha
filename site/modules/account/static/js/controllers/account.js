0901d7e030d6eb6ea63af91a8bd44aecc35d16a8
Antti Asi <antti.asi@guardtime.com>
2013-05-09 12:08:31 +0300

    Updated account summary page to display actual information

--- site/modules/account/static/js/controllers/account.js
+++ site/modules/account/static/js/controllers/account.js
@@ -6,5 +6,14 @@
         ['$scope', 'Account', 'localization', 'requestContext', 'BillingService', '$location', function ($scope, Account, localization, requestContext, BillingService, $location) {
             requestContext.setUpRenderContext('account.index', $scope);
             localization.bind('account', $scope);
+
+            $scope.account = Account.getAccount();
+            $scope.sshKeys = Account.getKeys(true);
+            $scope.paymentMethods = BillingService.getPaymentMethods();
+
+            $scope.$watch('account', function (acc) {
+                console.log(acc);
+            }, true);
+
         }]);
 }(window.JP.getModule('Account')));
