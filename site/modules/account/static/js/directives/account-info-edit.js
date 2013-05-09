794537043991983433ff7f215f1e77e9ff21b16b
Harri Siirak <harri.siirak@gmail.com>
2013-05-09 15:53:11 +0300

    Account information form validation

--- site/modules/account/static/js/directives/account-info-edit.js
+++ site/modules/account/static/js/directives/account-info-edit.js
@@ -2,51 +2,124 @@
 
 (function (app) {
 
-  app.directive('accountInfoEdit',['Account', '$q', function (Account, $q) {
+    app.directive('accountInfoEdit',
+        ['Account', 'localization', '$q',
+            function (Account, localization, $q) {
 
-    return {
-      restrict: 'A',
-      replace: true,
-      scope: true,
-      link: function ($scope) {
-        $scope.updateable = ['email','firstName','lastName','phone', 'companyName'];
-        $scope.updateable2 = ['address','postalCode','city','state','country'];
+                return {
+                    restrict: 'A',
+                    replace: true,
+                    scope: true,
+                    link: function ($scope) {
+                        $scope.fields = {
+                            basic: {
+                                email: {
+                                    title: localization.translate(null, 'account', 'Email'),
+                                    type: 'email',
+                                    validate: 'email',
+                                    required: true
+                                },
 
-        $scope.account = Account.getAccount(true);
-        $scope.setAccount = function() {
-          $scope.account = Account.getAccount(true);
-        }
+                                firstName: {
+                                    title: localization.translate(null, 'account', 'First name'),
+                                    type: 'text',
+                                    required: true
+                                },
 
-        $scope.error = null;
+                                lastName: {
+                                    title: localization.translate(null, 'account', 'Last name'),
+                                    type: 'text',
+                                    required: true
+                                },
 
-        $scope.saving = false;
+                                phone: {
+                                    title: localization.translate(null, 'account', 'Phone'),
+                                    type: 'number',
+                                    validate: 'phone',
+                                    pattern: '/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/',
+                                    required: true
+                                },
 
-        $scope.updateAccount = function () {
-          for(var field in $scope.updateable) {
-            if(!$scope.account.$$v[$scope.updateable[field]]) {
-              $scope.error = 'Please fill all the required fields';
-              return;
-            }
-          }
+                                companyName: {
+                                    title: localization.translate(null, 'account', 'Company name'),
+                                    type: 'text',
+                                    required: true
+                                }
+                            },
 
+                            address: {
+                                address: {
+                                    title: localization.translate(null, 'account', 'Address'),
+                                    type: 'text',
+                                    required: true
+                                },
 
-          $scope.saving = true;
-          console.log($scope.account);
-          Account.updateAccount($scope.account).then(function (newAccount) {
+                                postalCode: {
+                                    title: localization.translate(null, 'account', 'Postal code'),
+                                    type: 'number',
+                                    required: true
+                                },
 
-            $scope.setAccount();
-            $scope.saving = false;
-            $scope.error = null;
+                                city: {
+                                    title: localization.translate(null, 'account', 'City'),
+                                    type: 'text',
+                                    required: true
+                                },
 
-            $scope.nextStep();
-          }, function (err) {
-            $scope.error = null;
-            $scope.saving = false;
-          });
+                                state: {
+                                    title: localization.translate(null, 'account', 'State'),
+                                    type: 'text',
+                                    required: true
+                                },
 
-        };
-      },
-      templateUrl: 'account/static/partials/account-info-edit.html'
-    };
-  }]);
+                                country: {
+                                    title: localization.translate(null, 'account', 'Country'),
+                                    type: 'text',
+                                    required: true
+                                }
+                            }
+                        };
+
+                        $scope.error = null;
+                        $scope.saving = false;
+                        $scope.account = Account.getAccount(true);
+
+                        $scope.setAccount = function() {
+                            $scope.account = Account.getAccount(true);
+                        };
+
+                        $scope.updateAccount = function () {
+                            console.log($scope.account);
+                            function required(fields) {
+                                for(var field in fields) {
+                                    if (fields[field].required && !$scope.account.$$v[field]) {
+                                        return false;
+                                    }
+                                }
+
+                                return true;
+                            }
+
+                            if (required($scope.fields.basic) &&
+                                required($scope.fields.address)) {
+                                $scope.saving = true;
+
+                                Account.updateAccount($scope.account).then(function () {
+                                    $scope.setAccount();
+                                    $scope.saving = false;
+                                    $scope.error = null;
+
+                                    $scope.nextStep();
+                                }, function (err) {
+                                    $scope.error = null;
+                                    $scope.saving = false;
+                                });
+                            } else {
+                                $scope.error = localization.translate(null, 'account', 'Please fill all the required fields');
+                            }
+                        };
+                    },
+                    templateUrl: 'account/static/partials/account-info-edit.html'
+                };
+            }]);
 }(window.JP.getModule('Account')));
