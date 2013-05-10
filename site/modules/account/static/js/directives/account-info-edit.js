dfe8df36f81e0f2319663af93d3af1071891e8d9
Henrik Peinar <henrikpeinar@gmail.com>
2013-05-09 17:29:02 -0700

    added dropdown to account info signup flow

--- site/modules/account/static/js/directives/account-info-edit.js
+++ site/modules/account/static/js/directives/account-info-edit.js
@@ -2,140 +2,177 @@
 
 (function (app) {
 
-    app.directive('accountInfoEdit',
-        ['Account', 'localization', '$q',
-            function (Account, localization, $q) {
+  app.directive('accountInfoEdit',
+    ['Account', 'localization', '$q', '$http',
+      function (Account, localization, $q, $http) {
 
-                return {
-                    restrict: 'A',
-                    replace: true,
-                    scope: true,
-                    link: function ($scope) {
-                        function required(fields) {
-                            for(var field in fields) {
-                                if (fields[field].required && !$scope.account.$$v[field]) {
-                                    return false;
-                                }
-                            }
+        return {
+          restrict: 'A',
+          replace: true,
+          scope: true,
+          link: function ($scope) {
+            function required(fields) {
+              for(var field in fields) {
+                if (fields[field].required && !$scope.account.$$v[field]) {
+                  return false;
+                }
+              }
 
-                            return true;
-                        }
+              return true;
+            }
 
-                        function sanitize(fields) {
-                            for(var field in fields) {
-                                if (!fields[field].pattern) {
-                                    fields[field].pattern = /^+$/;
-                                }
-                            }
-                        }
+            function sanitize(fields) {
+              for(var field in fields) {
+                if (!fields[field].pattern) {
+                  fields[field].pattern = /^+$/;
+                }
+              }
+            }
 
-                        $scope.fields = {
-                            basic: {
-                                email: {
-                                    title: localization.translate(null, 'account', 'Email'),
-                                    type: 'email',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                },
+            $scope.countries = null;
+            $http.get('billing/countries').success(function (data) {
+              $scope.countries = data;
+            });
 
-                                firstName: {
-                                    title: localization.translate(null, 'account', 'First name'),
-                                    type: 'text',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                },
+            $scope.allStates = null;
+            $http.get('billing/states').success(function (data) {
+              $scope.allStates = data;
+            });
 
-                                lastName: {
-                                    title: localization.translate(null, 'account', 'Last name'),
-                                    type: 'text',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                },
+            $scope.stateSel = null;
 
-                                phone: {
-                                    title: localization.translate(null, 'account', 'Phone'),
-                                    type: 'text',
-                                    pattern: /^(\d+)$/,
-                                    required: true
-                                },
+            $scope.$watch('account["country"]', function (newVal, oldVal) {
+              console.log(newVal, oldVal, $scope.account);
+              if(oldVal === 'USA' || oldVal === 'CAN'){
+                $scope.account.$$v['state'] = '';
+              }
+              if(newVal === 'USA') {
+                $scope.stateSel = $scope.allStates.us.obj;
+              } else if (newVal === 'CAN') {
+                $scope.stateSel = $scope.allStates.canada.obj;
+              } else {
+                $scope.stateSel = undefined;
+              }
+            }, true);
 
-                                companyName: {
-                                    title: localization.translate(null, 'account', 'Company name'),
-                                    type: 'text',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                }
-                            },
 
-                            address: {
-                                address: {
-                                    title: localization.translate(null, 'account', 'Address'),
-                                    type: 'text',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                },
+            $scope.fields = {
+              basic: {
+                email: {
+                  title: localization.translate(null, 'account', 'Email'),
+                  type: 'email',
+                  shown: true,
+                  pattern: /^(.*)$/,
+                  required: true
+                },
 
-                                postalCode: {
-                                    title: localization.translate(null, 'account', 'Postal code'),
-                                    type: 'number',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                },
+                firstName: {
+                  title: localization.translate(null, 'account', 'First name'),
+                  type: 'text',
+                  shown: true,
+                  pattern: /^(.*)$/,
+                  required: true
+                },
 
-                                city: {
-                                    title: localization.translate(null, 'account', 'City'),
-                                    type: 'text',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                },
+                lastName: {
+                  title: localization.translate(null, 'account', 'Last name'),
+                  type: 'text',
+                  shown: true,
+                  pattern: /^(.*)$/,
+                  required: true
+                },
 
-                                state: {
-                                    title: localization.translate(null, 'account', 'State'),
-                                    type: 'text',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                },
+                phone: {
+                  title: localization.translate(null, 'account', 'Phone'),
+                  type: 'text',
+                  shown: true,
+                  pattern: /^(\d+)$/,
+                  required: true
+                },
 
-                                country: {
-                                    title: localization.translate(null, 'account', 'Country'),
-                                    type: 'text',
-                                    pattern: /^(.*)$/,
-                                    required: true
-                                }
-                            }
-                        };
+                companyName: {
+                  title: localization.translate(null, 'account', 'Company name'),
+                  type: 'text',
+                  shown: true,
+                  pattern: /^(.*)$/,
+                  required: true
+                }
+              },
 
-                        $scope.error = null;
-                        $scope.saving = false;
-                        $scope.account = Account.getAccount(true);
+              address: {
+                address: {
+                  title: localization.translate(null, 'account', 'Address'),
+                  type: 'text',
+                  shown: true,
+                  pattern: /^(.*)$/,
+                  required: true
+                },
 
-                        $scope.setAccount = function() {
-                            $scope.account = Account.getAccount(true);
-                        };
+                postalCode: {
+                  title: localization.translate(null, 'account', 'Postal code'),
+                  type: 'number',
+                  shown: true,
+                  pattern: /^(.*)$/,
+                  required: true
+                },
 
-                        $scope.updateAccount = function () {
-                            if (required($scope.fields.basic) &&
-                                required($scope.fields.address)) {
-                                $scope.saving = true;
+                city: {
+                  title: localization.translate(null, 'account', 'City'),
+                  type: 'text',
+                  shown: true,
+                  pattern: /^(.*)$/,
+                  required: true
+                },
 
-                                Account.updateAccount($scope.account).then(function () {
-                                    $scope.saving = false;
-                                    $scope.error = null;
+                state: {
+                  title: localization.translate(null, 'account', 'State'),
+                  type: 'text',
+                  shown: false,
+                  pattern: /^(.*)$/,
+                  required: true
+                },
 
-                                    if ($scope.nextStep) {
-                                        $scope.setAccount();
-                                        $scope.nextStep();
-                                    }
-                                }, function (err) {
-                                    $scope.error = null;
-                                    $scope.saving = false;
-                                });
-                            } else {
-                                $scope.error = localization.translate(null, 'account', 'Please fill all the required fields');
-                            }
-                        };
-                    },
-                    templateUrl: 'account/static/partials/account-info-edit.html'
-                };
-            }]);
+                country: {
+                  title: localization.translate(null, 'account', 'Country'),
+                  type: 'text',
+                  shown: false,
+                  pattern: /^(.*)$/,
+                  required: true
+                }
+              }
+            };
+
+            $scope.error = null;
+            $scope.saving = false;
+            $scope.account = Account.getAccount(true);
+
+            $scope.setAccount = function() {
+              $scope.account = Account.getAccount(true);
+            };
+
+            $scope.updateAccount = function () {
+              if (required($scope.fields.basic) &&
+                required($scope.fields.address)) {
+                $scope.saving = true;
+
+                Account.updateAccount($scope.account).then(function () {
+                  $scope.saving = false;
+                  $scope.error = null;
+
+                  if ($scope.nextStep) {
+                    $scope.setAccount();
+                    $scope.nextStep();
+                  }
+                }, function (err) {
+                  $scope.error = null;
+                  $scope.saving = false;
+                });
+              } else {
+                $scope.error = localization.translate(null, 'account', 'Please fill all the required fields');
+              }
+            };
+          },
+          templateUrl: 'account/static/partials/account-info-edit.html'
+        };
+      }]);
 }(window.JP.getModule('Account')));
