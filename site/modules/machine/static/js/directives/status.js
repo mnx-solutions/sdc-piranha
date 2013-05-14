50b48fbb5de28eb3690ba3ead632bcb50c95e8e3
thomas m. vaht <zammalhabe@mac.com>
2013-05-14 15:34:40 -0700

    machine list statuses

--- site/modules/machine/static/js/directives/status.js
+++ site/modules/machine/static/js/directives/status.js
@@ -21,19 +21,21 @@
                     }
                 };
             },
+			//<span class="status label label-info">Started</span>
             template: '<span class="status label label-info" ng-class="labelForState(machine.state)" ng-show="!machine.job || machine.job.finished">' +
                 '{{machine.state}}</span>' +
-                '<span class="status label" ng-show="machine.job && !machine.job.finished">' +
-                '<img src="/static/img/ajax-loader.gif"/>' +
-                '<span data-ng-show="machine.job.name == \'MachineCreate\'" data-translate>Creating</span>' +
-                '<span data-ng-show="machine.job.name == \'MachineStart\'" data-translate>Starting</span>' +
-                '<span data-ng-show="machine.job.name == \'MachineStop\'" data-translate>Stopping</span>' +
-                '<span data-ng-show="machine.job.name == \'MachineResize\'" data-translate>Resizing</span>' +
-                '<span data-ng-show="machine.job.name == \'MachineReboot\'" data-translate>Rebooting</span>' +
-                '<span data-ng-show="machine.job.name == \'MachineDelete\'" data-translate>Deleting</span>' +
+                '<span class="loading-small" ng-show="machine.job && !machine.job.finished">' +
+                // '<img src="/static/img/ajax-loader.gif"/>' +
+                // '<span data-ng-show="machine.job.name == \'MachineCreate\'" data-translate></span>' +//Creating
+                // '<span data-ng-show="machine.job.name == \'MachineStart\'" data-translate></span>' +//Starting
+                // '<span data-ng-show="machine.job.name == \'MachineStop\'" data-translate></span>' +//Stopping
+                // '<span data-ng-show="machine.job.name == \'MachineResize\'" data-translate></span>' +//Resizing
+                // '<span data-ng-show="machine.job.name == \'MachineReboot\'" data-translate></span>' +//Rebooting
+                // '<span data-ng-show="machine.job.name == \'MachineDelete\'" data-translate></span>' +//Deleting
                 '</span>' +
-                '<span class="status label label-important" ng-show="machine.job.err"><i class="icon-warning-sign icon-white border"/>' +
-                '</span>'
+				'<i class="icon-warning-sign icon-white" ng-show="machine.job.err"></i>'
+                // '<span class="status label label-important" ng-show="machine.job.err"><i class="icon-warning-sign icon-white border"/>' +
+                // '</span>'
         };
     });
 }(window.JP.getModule('Machine')));
