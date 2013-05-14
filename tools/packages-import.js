bf7f65ec987e6d32bd771fc57ab45b393b68cf50
endrikend <endrik.end@gmail.com>
2013-05-14 16:44:08 -0700

    packages prices to rounded up floats

--- tools/packages-import.js
+++ tools/packages-import.js
@@ -6,7 +6,10 @@ var result = {};
 csv()
     .from.stream(fs.createReadStream(process.argv[2] || __dirname+'/packages.csv'), {columns: true})
     .on('record', function(row, index){
-        result[row["Joyent Instant Type Name"]]= {type: row.TYPE, group: row["Pricing Group"], description: row.Description, price: row.Price, price_month: row["Price Per Month"]};
+        // turning price strings to floats and round up to nearest penny;
+        var price = Math.ceil(parseFloat(row.Price.split(',').join('')) * 100) / 100;
+        var price_month = Math.ceil(parseFloat(row["Price Per Month"].split(',').join('')) * 100) / 100;
+        result[row["Joyent Instant Type Name"]]= {type: row.TYPE, group: row["Pricing Group"], description: row.Description, price: price, price_month: price_month};
     })
     .on('end', function(count){
         console.log(JSON.stringify(result, null, " "));
