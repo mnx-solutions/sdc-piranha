222e078193aa0192fa0a034d7b3f33891f5b6632
Harri Siirak <harri.siirak@gmail.com>
2013-05-14 16:42:29 +0300

    Remove bogus logging

--- site/modules/server/static/js/services/tab.js
+++ site/modules/server/static/js/services/tab.js
@@ -73,15 +73,11 @@
                                 self.results(data);
                                 setTimeout(self.poll, 100);
                             } else if (code === 204) {
-                                console.log('nothing processing');
                                 if (Object.keys(_calls).length) {
-                                    console.log('ERROR: Out of sync');
-                                    console.log(_calls);
                                 }
                             }
                         })
                         .error(function() {
-                            console.log(arguments);
                             polling = false;
                             eventer.$emit('error');
                         });
