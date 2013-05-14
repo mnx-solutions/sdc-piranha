9b8da6636423548d46deaa1af581257cd57e20b3
Harri Siirak <harri.siirak@gmail.com>
2013-05-14 16:56:05 +0300

    Disable provision button if machine name is not provided

--- site/modules/machine/static/js/directives/name.js
+++ site/modules/machine/static/js/directives/name.js
@@ -8,7 +8,8 @@
                 var machines = Machine.machine();
 
                 ctrl.$parsers.unshift(function(viewValue) {
-                    if (/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(viewValue)) {
+                    if (viewValue.length > 0 &&
+                        /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(viewValue)) {
                         if(!machines.some(function (m) { return m.name === viewValue; })) {
                             ctrl.$setValidity('machineName', true);
                             return viewValue;
