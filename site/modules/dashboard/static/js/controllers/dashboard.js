65a68cfb05b75b8db828cb452da719731661852d
Jaan Oras <jaan.oras@guardtime.com>
2013-05-15 14:20:50 +0300

    Refactored dashboard code.
    Now is easier to read and fully based on promises.

--- site/modules/dashboard/static/js/controllers/dashboard.js
+++ site/modules/dashboard/static/js/controllers/dashboard.js
@@ -18,32 +18,34 @@
             function ($scope, $$track, $dialog, $q, requestContext, Account, Zendesk, Machine, localization, util, BillingService) {
                 localization.bind('dashboard', $scope);
                 requestContext.setUpRenderContext('dashboard.index', $scope);
-
                 $scope.loading = true;
-                var c = 0;
-                function checkAll() {
-                    if(++c > 3) {
-                        $scope.loading = false;
-                    }
-                }
 
-                $scope.account = Account.getAccount();
-                $q.when($scope.account, checkAll);
-
+                // populate all datasources
+                $scope.account     = Account.getAccount();
+                $scope.forums      = Zendesk.getForumsList();
+                $scope.machines    = Machine.machine();
+                $scope.lastInvoice = BillingService.getLastInvoice();
                 parseRSS('http://joyent.com/blog/feed', function(response) {
                     $scope.rssentries = response.entries;
                 });
 
-                $scope.forums = Zendesk.getForumsList();
-                $q.when($scope.forums, checkAll);
+                // when all datasources are loaded, disable loader
+                $q.all(
+                   [$q.when($scope.machines),
+                    $q.when($scope.lastInvoice),
+                    $q.when($scope.forums),
+                    $q.when($scope.account),
+                    $q.when($scope.rssentries)
+                ]).then( function(){
+                    $scope.loading = false;
+                });
 
-                var runningcount = 0;
-                var othercount = 0;
 
-                $scope.machines = Machine.machine();
-                $q.when($scope.machines, checkAll);
-
+                // count running/not running machines
                 $scope.$watch('machines', function (machines) {
+                    var runningcount = 0;
+                    var othercount = 0;
+
                     machines.forEach(function (machine) {
                         if (machine.state === 'running') {
                             runningcount += 1;
@@ -51,14 +53,13 @@
                             othercount += 1;
                         }
                     });
+
                     $scope.runningcount = runningcount;
                     $scope.othercount = othercount;
                 }, true);
 
-                $scope.runningcount = runningcount;
-                $scope.othercount = othercount;
-                $scope.lastInvoice = BillingService.getLastInvoice();
-                $q.when($scope.lastInvoice, checkAll);
+                $scope.runningcount = 0;
+                $scope.othercount = 0;
             }
 
         ]);
