d4a7da0a4319d897d8ab28578e8e4dca06a59ce2
Karl Düüna <karl.dyyna@gmail.com>
2013-05-15 13:47:09 -0700

    Unify support links

--- site/modules/menu/static/js/directives/main-menu.js
+++ site/modules/menu/static/js/directives/main-menu.js
@@ -30,7 +30,7 @@
 								'<a href="#!/account/">My Account</a>' +
 							'</div>' +
                             '<div class="pull-right top-menu">' +
-									'<span><a href="http://joyent.com/developers/support" target="_blank">Support</a></span>' +
+									'<span><a href="http://myaccount.zendesk.com/account/dropboxes/20175628" onclick="script:Zenbox.show(); return false;">Support</a></span>' +
 									'<span><a href="http://wiki.joyent.com/wiki/display/jpc2/JoyentCloud+Home" target="_blank">Docs</a></span>' +
 									// '<span><a href="#!/account">My Account</a></span>' +
 									'<span class="last"><a href="/landing/forgetToken">Log Out</a></span>' +
