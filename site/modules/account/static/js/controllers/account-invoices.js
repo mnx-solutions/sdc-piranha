5f074c87d8bc67dae14da9a8bf4cee8ecc394f56
Karl Düüna <karl.dyyna@gmail.com>
2013-05-15 12:13:57 -0700

    Initializing->loading invoices

--- site/modules/account/static/js/controllers/account-invoices.js
+++ site/modules/account/static/js/controllers/account-invoices.js
@@ -6,7 +6,13 @@
         ['$scope', 'requestContext', '$http', 'BillingService', function ($scope, requestContext, $http, BillingService) {
             requestContext.setUpRenderContext('account.invoices', $scope);
 
+            $scope.loading = false;
             $scope.invoices = BillingService.getInvoices();
+
+            $scope.invoices.then(function () {
+                $scope.loading = false;
+            });
+
 
             $scope.openDetails = null;
             $scope.setOpenDetails = function(id) {
