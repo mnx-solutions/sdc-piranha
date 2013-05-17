770fd0f98322e74b92d3b61c418ecf59064b6c1f
endrikend <endrik.end@gmail.com>
2013-05-16 17:02:26 -0700

    use strict error fixes

--- site/modules/cloudAnalytics/static/vendor/rickshaw/rickshaw.js
+++ site/modules/cloudAnalytics/static/vendor/rickshaw/rickshaw.js
@@ -936,7 +936,7 @@ Rickshaw.Fixtures.Time = function() {
 Rickshaw.namespace('Rickshaw.Fixtures.Number');
 
 Rickshaw.Fixtures.Number.formatKMBT = function(y) {
-	abs_y = Math.abs(y);
+	var abs_y = Math.abs(y);
 	if (abs_y >= 1000000000000)   { return y / 1000000000000 + "T" } 
 	else if (abs_y >= 1000000000) { return y / 1000000000 + "B" } 
 	else if (abs_y >= 1000000)    { return y / 1000000 + "M" } 
@@ -947,7 +947,7 @@ Rickshaw.Fixtures.Number.formatKMBT = function(y) {
 };
 
 Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
-    abs_y = Math.abs(y);
+    var abs_y = Math.abs(y);
     if (abs_y >= 1125899906842624)  { return y / 1125899906842624 + "P" }
     else if (abs_y >= 1099511627776){ return y / 1099511627776 + "T" }
     else if (abs_y >= 1073741824)   { return y / 1073741824 + "G" }
