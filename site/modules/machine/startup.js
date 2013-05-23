65d264eaa54c64138e1e82707784de1aca65efd7
endrikend <endrik.end@gmail.com>
2013-05-23 15:29:07 -0700

    license logging

--- site/modules/machine/startup.js
+++ site/modules/machine/startup.js
@@ -183,7 +183,9 @@ module.exports = function (scope, callback) {
                     for(var k in info.licenses["License Portfolio"]) {
                         var lic = info.licenses["License Portfolio"][k];
                         if(lic["API Name"] == data[i].name) {
+
                             data[i].license_price = lic["Pan-Instance Price Uplift"];
+                            console.log('match found for dataset license', data[i]);
                         }
                     }
                 }
