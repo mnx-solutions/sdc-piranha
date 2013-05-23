abb772cddf89f0c20974b491214a02adb4abd790
endrikend <endrik.end@gmail.com>
2013-05-23 16:54:58 -0700

    license handle commas

--- site/modules/machine/static/js/controllers/provision.js
+++ site/modules/machine/static/js/controllers/provision.js
@@ -169,12 +169,12 @@
                             if(lPrice === lPrice) {
                                 $scope.packages.forEach(function(p) {
 
-                                    if(!p.full_price && p.price && parseFloat(p.price) === parseFloat(p.price)) {
-                                        p.full_price = lPrice + parseFloat(p.price);
+                                    if(!p.full_price && p.price) {
+                                        p.full_price = lPrice + parseFloat(p.price.split(',').join(''));
                                         p.full_price = p.full_price.toFixed(3);
                                     }
-                                    if(!p.full_price_month && p.price_month && parseFloat(p.price_month) === parseFloat(p.price_month)) {
-                                        p.full_price_month = parseFloat(p.price_month) + (lPrice * 730);
+                                    if(!p.full_price_month && p.price_month) {
+                                        p.full_price_month = parseFloat(p.price_month.split(',').join('')) + (lPrice * 730);
                                         p.full_price_month = p.full_price_month.toFixed(2);
                                     }
                                 });
