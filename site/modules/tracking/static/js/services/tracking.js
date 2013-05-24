0defcdf933fbd46cde7f0a499850776e0f8965e8
Henrik Peinar <henrikpeinar@gmail.com>
2013-05-23 20:38:47 -0600

    Another marketo field fix

--- site/modules/tracking/static/js/services/tracking.js
+++ site/modules/tracking/static/js/services/tracking.js
@@ -27,7 +27,7 @@
                             FirstName:         account.firstName,
                             LastName:          account.lastName,
                             Company:           account.companyName || '--',
-                            CAPI_UUID_L:       account.id || '',
+                            CAPI_UUID__c_lead: account.id || '',
                             Campaign_ID__c:    $cookies.campaignId || '70180000000ShEu'
                         },
                         enc_email
