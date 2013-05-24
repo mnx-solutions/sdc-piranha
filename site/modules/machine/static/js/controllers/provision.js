47eefe976eb3fb18db30f58cbc5b0c47d2a58144
Karl Düüna <karl.dyyna@gmail.com>
2013-05-23 18:12:38 -0700

    Fix false.toFixed

--- site/modules/machine/static/js/controllers/provision.js
+++ site/modules/machine/static/js/controllers/provision.js
@@ -337,8 +337,10 @@
                                 if(packageTypes.indexOf(p.group) === -1){
                                     packageTypes.push(p.group);
                                 }
-                                p.price = getNr(p.price).toFixed(3);
-                                p.price_month = getNr(p.price_month).toFixed(2);
+                                var price = getNr(p.price);
+                                var priceMonth = getNr(p.price_month);
+                                p.price = price && price.toFixed(3) || undefined;
+                                p.price_month = priceMonth && priceMonth.toFixed(2) || undefined;
                             });
                             $scope.packageTypes = packageTypes;
                             $scope.packages = packages;
