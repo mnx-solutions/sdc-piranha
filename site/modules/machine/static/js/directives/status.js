6d6cf4d6fd9f8bf117eda58929354ca7ac11c436
Harri Siirak <harri.siirak@gmail.com>
2013-06-07 12:49:11 +0300

    PIRANHA-393 Cleanup machine status directive

--- site/modules/machine/static/js/directives/status.js
+++ site/modules/machine/static/js/directives/status.js
@@ -1,33 +1,39 @@
 'use strict';
 
 (function (app) {
-
-    app.directive('machineStatus', function () {
-
+    app.directive('machineStatus', [ 'localization', function (localization) {
         return {
             restrict: 'E',
             replace: true,
             scope: {
                 machine: '=machine'
             },
+
+            controller: function($scope, $element, $attrs, $transclude) {
+                localization.bind('machine', $scope);
+            },
+
             link: function (scope) {
                 scope.labelForState = function (state) {
                     switch (state) {
+                        case 'provisioning':
+                        case 'stopping':
+                            return 'btn-warning';
+                            break;
+
                         case 'running':
                             return 'btn-success';
+
                         case 'stopped':
                             return '';
+
                         default:
                             return 'btn-danger';
                     }
                 };
             },
-			//<span class="status label label-info">Started</span>
-            template: '<div><span class="status label label-info" ng-class="labelForState(machine.state)" ng-show="!machine.job || machine.job.finished">' +
-                '{{machine.state}}</span>' +
-                '<span class="loading-small" ng-show="machine.job && !machine.job.finished">' +
-                '</span>' +
-				'<i class="icon-warning-sign icon-white" ng-show="machine.job.err"></i></div>'
+
+            templateUrl: 'machine/static/partials/status.html'
         };
-    });
+    }]);
 }(window.JP.getModule('Machine')));
