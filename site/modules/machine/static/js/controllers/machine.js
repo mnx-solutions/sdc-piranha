1aa572ef5ad350d30aa566597089c610c592d6e1
Henrik Peinar <henrikpeinar@gmail.com>
2013-06-13 12:14:55 -0700

    Rename now starts off with old name

--- site/modules/machine/static/js/controllers/machine.js
+++ site/modules/machine/static/js/controllers/machine.js
@@ -242,6 +242,11 @@
                     });
                 };
 
+                $scope.enableRename = function(name) {
+                    $scope.changingName = true;
+                    $scope.newInstanceName == name;
+                };
+
                 $scope.clickRename = function() {
                     util.confirm(
                         localization.translate(
