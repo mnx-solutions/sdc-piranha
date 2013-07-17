1fcced43679a8bcab6089e204e8d1d5a67629cd3
Harri Siirak <harri.siirak@gmail.com>
2013-07-17 12:41:15 +0300

    Code cleanup

--- site/auth.js
+++ site/auth.js
@@ -4,13 +4,14 @@ module.exports = function execute(scope) {
     var smartCloud = scope.get('smartCloud');
 
     return function (req, res, next) {
-        if(!req.session.token) {
+        if (!req.session.token) {
             // token missing, don't allow the request
 
             res.send(401);
             return;
         }
-        if(req.session.userId && req.session.userName) {
+
+        if (req.session.userId && req.session.userName) {
             req.log = req.log.child({userName: req.session.userName, userId: req.session.userId});
         }
 
@@ -18,21 +19,23 @@ module.exports = function execute(scope) {
         if(!req.cloud) {
             var _cloud = null;
             Object.defineProperty(req, 'cloud', {
-                get: function() {
-                    if(!_cloud) {
+                get: function () {
+                    if (!_cloud) {
                         _cloud = smartCloud.cloud({ token: req.session.token });
                     }
                     return _cloud;
                 },
+
                 enumerable: true
             });
 
-            if(smartCloud.needRefresh()) {
+            if (smartCloud.needRefresh()) {
                 smartCloud.cloud({token: req.session.token}, function (err, cloud) {
                     if (err) {
                         next(err);
                         return;
                     }
+                    
                     _cloud = cloud;
                     next();
                 });
