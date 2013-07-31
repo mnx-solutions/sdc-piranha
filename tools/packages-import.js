1a54ca945b71ab86dc2c0e40ffc9b187c4d5f307
unknown <henrikpeinar@gmail.com>
2013-07-31 14:42:01 +0300

    Fixed packages.csv convert script

--- tools/packages-import.js
+++ tools/packages-import.js
@@ -1,16 +1,33 @@
 var fs = require('fs');
 var csv = require('csv');
+var async = require('async');
 
 var result = {};
 
-csv()
-    .from.stream(fs.createReadStream(process.argv[2] || __dirname+'/packages.csv'), {columns: true})
-    .on('record', function(row, index){
-        result[row["Joyent Instant Type Name"]]= {short_name:row["Short Name"], type: row.TYPE, group: row["Pricing Group"], description: row.Description, price: row.Price, price_month: row["Price Per Month"], vcpus: row.VCPUS};
-    })
-    .on('end', function(count){
-        console.log(JSON.stringify(result, null, " "));
-    })
-    .on('error', function(error){
-        console.log(error.message);
-    });
+fs.readFile((process.argv[2] || __dirname+'/packages.csv'), function(err, data) {
+	csv()
+		.from.string(data)
+		.to.array(function(results) {
+			
+				async.each(results, function(results, cb) {
+						result[results[7]]= {
+							short_name: results[6],
+							type: results[8],
+							group: results[14],
+							description: results[13],
+							price: results[15],
+							price_month: results[16],
+							vcpus: results[11]
+						};
+						
+						cb(null);
+				}, function(err) {
+					console.log(JSON.stringify(result, null, " "));
+				});
+
+		}).on('end', function(count) {
+			
+		}).on('error', function(error){
+			console.log(error.message);
+		});
+});
