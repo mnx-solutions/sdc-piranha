75f65f3876f5532fbc2c94ce151d6f00676580b7
Alexander Rysenko <alex.rysenko@silvertreesystems.com>
2013-09-02 20:15:53 +0300

    [PIRANHA-561] Adds MaxMind test

---
+++ site/modules/signup/static/js/services/maxmind.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function (app) {
+    app.factory('MaxMind', [
+        '$http',
+        '$q',
+        function ($http) {
+            var service = {};
+            service.getCountries = function () {
+                return $http.get('account/countryCodes');
+            };
+            service.makeCall = function(phone) {
+                return $http.get('/signup/signup/maxmind/call/%2B' + phone);
+            };
+            service.verify = function (pin) {
+                return $http.get('/signup/signup/maxmind/verify/' + pin);
+            };
+            return service;
+        }]);
+}(window.JP.getModule('Signup')));
--- test/spec/MaxMindSpec.js
+++ test/spec/MaxMindSpec.js
@@ -4,7 +4,9 @@ describe('MaxMind', function() {
         beforeEach(module('JoyentPortal'));
         beforeEach(inject(function ($rootScope, $controller) {
             scope = $rootScope.$new();
-            ctrl = $controller('MaxMindController', {$scope: scope});
+            var account = new AccountMock();
+            var maxMindService = new MaxMindMock();
+            ctrl = $controller('MaxMindController', {$scope: scope, Account: account, MaxMind: maxMindService});
         }));
         it('basic test', function() {
             expect(2).toBe(2);
