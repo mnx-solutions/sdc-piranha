27c8542f753833882aff50ce2f127541de721dbc
Henrik Peinar <henrikpeinar@gmail.com>
2013-10-07 15:47:19 +0300

    PIRANHA-703 Fully reworked SSH key functionality. Import / create is now in directives. Both account and signup ssh pages now use modals. Changed signup ssh keys page to match  20130930-ssh-keys.pdf.

---
+++ site/modules/account/static/js/directives/account-ssh-create.js
@@ -0,0 +1,97 @@
+'use strict';
+
+(function (app) {
+
+    app.directive('accountSshCreate', [
+        'Account',
+        'localization',
+        'notification',
+        '$q',
+        '$window',
+        '$dialog',
+        '$timeout',
+        '$http',
+        function (Account, localization, notification, $q, $window, $dialog, $timeout, $http) {
+            return {
+                restrict: 'EA',
+                replace: true,
+                scope: true,
+                controller: function($scope, $element, $attrs, $transclude) {
+                    localization.bind('account', $scope);
+
+                    $scope.loading = false;
+
+                },
+
+                link: function ($scope) {
+                    /* ssh key generating popup with custom template */
+                    var sshKeyModalCtrl = function($scope, dialog) {
+                        $scope.keyName = '';
+
+                        $scope.close = function(res) {
+                            dialog.close(res);
+                        };
+
+                        $scope.generateKey = function() {
+                            $scope.close({generate: true, keyName: $scope.keyName});
+                        };
+                    };
+
+                    var generateKeyPopup = function(question, callback) {
+                        var title = 'Create SSH Key';
+                        var btns = [{result:'cancel', label:'Cancel', cssClass: 'pull-left'}];
+                        var templateUrl = 'account/static/template/dialog/generate-ssh-modal.html';
+
+                        $dialog.messageBox(title, question, btns, templateUrl)
+                            .open(templateUrl, sshKeyModalCtrl)
+                            .then(function(data) {
+                                if(data && data.generate === true) {
+                                    var createUrl = '/main/account/ssh/create/';
+
+                                    if($scope.nextStep) {
+                                        createUrl = '/signup/account/ssh/create/';
+                                    }
+
+                                    $http.post(createUrl, {name: data.keyName})
+                                        .success(function(data) {
+                                            if(data.success === true) {
+                                                notification.push(null, { type: 'success' },
+                                                    localization.translate($scope, null,
+                                                        'SSH key successfully added to your account<br />You will be prompted for private key download shortly. Please keep your private key safe'
+                                                    )
+                                                );
+                                                $window.open('http://'+ window.location.host +'/main/account/ssh/download/'+ data.keyId +'/'+ data.name +'');
+
+                                                if($scope.updateInterval) {
+                                                    $scope.updateInterval();
+                                                }
+
+                                                if($scope.nextStep) {
+                                                    window.location.href = '/main';
+                                                }
+                                            } else {
+                                                // error
+                                                notification.push(null, { type: 'error' },
+                                                    localization.translate($scope, null,
+                                                        'Unable to generate SSH key: '+ data.err.message
+                                                    )
+                                                );
+                                            }
+                                        });
+
+                                }
+                            });
+                    };
+
+                    /* SSH Key generation popup */
+                    $scope.generateSshKey = function() {
+                        generateKeyPopup('', function(keyData){
+
+                        });
+                    };
+
+                },
+                templateUrl: 'account/static/partials/account-ssh-create.html'
+        };
+}]);
+}(window.JP.getModule('Account')));
