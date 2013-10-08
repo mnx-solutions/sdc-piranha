4a1f7d5001f1a8ada624f1e8b97a521a67d8d586
Alexander Rysenko <rysenko@gmail.com>
2013-10-08 18:24:37 +0300

    PIRANHA-731 ELB Create/Update UI - Fix navigation from list to detail instead of edit page

--- site/modules/elb/static/js/controllers/elb-detail.js
+++ site/modules/elb/static/js/controllers/elb-detail.js
@@ -2,12 +2,21 @@
 
 (function (app) {
     app.controller(
-        'elb.EditController',
-        ['$scope', 'requestContext', 'localization', function ($scope, requestContext, localization) {
+        'elb.DetailController',
+        ['$scope', 'requestContext', 'localization', '$resource', '$location',
+                function ($scope, requestContext, localization, $resource, $location) {
             localization.bind('elb', $scope);
             requestContext.setUpRenderContext('elb.detail', $scope, {
                 title: localization.translate(null, 'elb', 'Load Balancer Details')
             });
+
+            var balancerId = requestContext.getParam('balancerId');
+            var resource = $resource('elb/item/:id', {id:'@id'});
+            $scope.server = resource.get({id: balancerId});
+
+            $scope.edit = function () {
+                $location.path('/elb/edit/' + $scope.server.id);
+            };
 
         }]);
 }(window.JP.getModule('elb')));
