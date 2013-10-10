74be03a5211e3775f6ab86911dc02ae4e57b6535
Dmitry Shkilyov <dima@sfdev.com>
2013-10-10 14:10:14 +0300

    add loading indicator to list.html

--- site/modules/elb/static/js/controllers/elb-list.js
+++ site/modules/elb/static/js/controllers/elb-list.js
@@ -4,14 +4,17 @@
     app.controller(
         'elb.ListController',
         ['$scope', 'requestContext', 'localization', 'elb.Service', function ($scope, requestContext, localization, service) {
+            $scope.listLoaded = false;
             localization.bind('elb', $scope);
             requestContext.setUpRenderContext('elb.list', $scope, {
                 title: localization.translate(null, 'elb', 'Load Balancers List')
             });
 
             $scope.servers = [];
+
             service.getBalancers().then(function (data) {
                 $scope.servers = data;
+                $scope.listLoaded = true;
             });
 
         }]);
