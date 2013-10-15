7475df3ff78e8d306db3ab0810ba652698c81944
Alexander Rysenko <rysenko@gmail.com>
2013-10-15 17:17:01 +0300

    PIRANHA-731 Certificates upload/parse

--- site/modules/elb/static/js/directives/cert-upload.js
+++ site/modules/elb/static/js/directives/cert-upload.js
@@ -4,7 +4,7 @@
     app.directive('certUpload', ['notification', function (notification) {
         return {
             templateUrl: 'elb/static/partials/cert-upload.html',
-            restrict: 'E',
+            restrict: 'EA',
             scope: {
                 model: '='
             },
@@ -18,14 +18,20 @@
 
                 function uploadCertificate(file) {
                     var data = new FormData(), xhr = new XMLHttpRequest();
-                    xhr.onloadstart = function (e) {
-                        // start
-                    };
                     xhr.onerror = function (e) {
-                        // error
+                        scope.$apply(function () {
+                            notification.replace('elb', { type: 'error' }, 'Error while uploading certificate');
+                        });
                     };
                     xhr.onload = function (e) {
-                        console.log(xhr.responseText);
+                        scope.$apply(function () {
+                            if (xhr.status == 200) {
+                                notification.replace('elb', { type: 'success' }, 'Certificate added');
+                                scope.model = JSON.parse(xhr.responseText).id;
+                            } else {
+                                notification.replace('elb', { type: 'error' }, xhr.responseText);
+                            }
+                        });
                     };
                     data.append('certificate', file, file.name);
                     xhr.open('POST', '/main/elb/certificates');
