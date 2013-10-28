35e935a7445071930e5c68e11a23360bc3d46066
Alexander Rysenko <rysenko@gmail.com>
2013-10-28 19:07:44 +0200

    Fix according to code review

--- site/modules/elb/app.js
+++ site/modules/elb/app.js
@@ -33,32 +33,45 @@ module.exports = function execute(scope, app) {
         return null;
     }
 
+    function getUploadResult(callback, resultObj) {
+        return '<script language="javascript" type="text/javascript">' +
+            callback + '(' + JSON.stringify(resultObj) + ');' +
+            '</script>';
+    }
+
+    // This is needed as source for upload target iframe
+    app.get('/blank', function (req, res) {
+        res.send('');
+    });
+
     app.post('/certificates', function (req, res) {
+        var callback = req.query.callback;
         var form = new multiparty.Form();
         form.parse(req, function handleForm(err, fieldsObject, filesObject) {
             if (err || !filesObject.certificate) {
-                res.send(400, 'Certificate not found');
+                res.send(getUploadResult(callback, {success: false, message: 'Certificate not found'}));
                 return;
             }
             var data = {};
             var pemSrc = fs.readFileSync(filesObject.certificate.path, 'utf8');
             data['private'] = parsePemSection(pemSrc, 'RSA PRIVATE KEY');
             if (!data['private']) {
-                res.send(400, 'Private key not found in PEM');
+                res.send(getUploadResult(callback, {success: false, message: 'Private key not found in PEM'}));
                 return;
             }
             pem.getPublicKey(pemSrc, function (err, publicKey) {
                 if (err) {
-                    res.send(400, 'Public key not found in PEM: ' + err);
+                    res.send(getUploadResult(callback, {success: false, message: 'Public key not found in PEM: ' + err}));
                     return;
                 }
                 data['public'] = publicKey;
                 client.post('/certificates', data, function (err, creq, cres, obj) {
                     if (err) {
-                        res.send(400, err);
+                        res.send(getUploadResult(callback, {success: false, message: err}));
                         return;
                     }
-                    res.json(obj);
+                    obj.success = true;
+                    res.send(getUploadResult(callback, obj));
                 });
             });
         });
