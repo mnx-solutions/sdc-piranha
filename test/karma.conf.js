514b30630242c6719206f22e3b11ada72559a5d6
Harri Siirak <harri.siirak@gmail.com>
2013-11-08 11:36:54 +0200

    PIRANHA-875 Karma & Protractor configuration files

---
+++ test/karma.conf.js
@@ -0,0 +1,41 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
+        frameworks: [ 'jasmine' ],
+
+        files: [
+            "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js",
+            "../site/static/vendor/angular/angular.js",
+            "site/static/vendor/angular/angular-resource.js",
+            "site/static/vendor/angular/angular-cookies.js",
+            "site/static/vendor/angular/angular-mocks.js",
+            "site/static/vendor/zendesk/zenbox.js",
+            "site/static/vendor/bootstrap/js/angular-ui.min.js",
+            "site/static/vendor/bootstrap/js/bootstrap.min.js",
+            "site/static/vendor/bootstrap/js/dialog.js",
+            "site/static/vendor/bootstrap/js/transition.js",
+            "site/static/vendor/moment/moment.min.js",
+            "site/static/vendor/uuid/uuid.js",
+
+            "site/static/js/jp.js",
+            "site/modules/**/static/js/module.js",
+            "site/static/js/*.js",
+            "site/static/js/**/*.js",
+            "site/modules/**/static/js/**/*.js",
+            "site/modules/**/static/vendor/**/*.js",
+            "**/modules/machine/**/test/mock/*.js",
+
+            "site/modules/**/test/*.spec.js"
+        ],
+
+        singleRun: true,
+        browsers: [ 'Chrome' ],
+        reporters: 'progress',
+
+        plugins: [
+            'karma-jasmine',
+            'karma-phantomjs-launcher',
+            'karma-chrome-launcher'
+        ]
+    });
+};
