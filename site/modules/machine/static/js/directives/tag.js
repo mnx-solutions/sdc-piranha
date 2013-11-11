50ac4178585508ab13652aa92652043558365652
Karl Düüna <karl.dyyna@gmail.com>
2013-11-11 16:39:13 +0200

    PIRANHA-880 - rewrite tag handling on client-side

--- site/modules/machine/static/js/directives/tag.js
+++ site/modules/machine/static/js/directives/tag.js
@@ -5,19 +5,32 @@
         function () {
             return {
                 require: 'ngModel',
-                link: function (scope, elm, attrs, ctrl) {
+                link: function ($scope, elm, attrs, ctrl) {
                     ctrl.$parsers.unshift(function (viewValue) {
-                        if ((viewValue && viewValue.length > 0)) {
-                            if (!/^[A-Z0-9\._-]*$/i.test(viewValue)) {
-                                ctrl.$setValidity('tag', false);
-                            } else {
-                                ctrl.$setValidity('tag', true);
-                            }
-                        } else {
+
+                        // Not empty
+                        if (!viewValue || viewValue.length === 0) {
+                            // Both empty key and val are valid
+                            ctrl.$setValidity('tag', (!$scope.o.key || !$scope.o.val));
+                            return viewValue;
+                        }
+                        // No invalid characters
+                        if (/[^A-Z0-9\._\-]/i.test(viewValue)) {
                             ctrl.$setValidity('tag', false);
+                            return viewValue;
+                        }
+                        // Is unique key
+                        var notUnique = $scope.tagsArray.some(function (tag, index) {
+                            if (tag.key === $scope.o.key && index !== $scope.key) {
+                                return true;
+                            }
+                        });
+                        if (notUnique) {
+                            ctrl.$setValidity('tag', false);
+                            return viewValue;
                         }
 
-
+                        ctrl.$setValidity('tag', true);
                         return viewValue;
                     });
                 }
