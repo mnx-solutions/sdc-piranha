45ea19f797e0351b319b982db3c8c610d47e5e07
Alexander Rysenko <alex.rysenko@silvertreesystems.com>
2013-11-12 19:09:53 +0200

    JP-204 Created date shows "NaNs" when provisioning

--- site/apps/main/static/js/filters/dateTime.js
+++ site/apps/main/static/js/filters/dateTime.js
@@ -2,6 +2,9 @@
 
 window.JP.main.filter('dateTime', function () {
     return function (dateString, expression) {
+        if (!dateString) {
+            return '';
+        }
         return window.moment(new Date(dateString)).format(expression || 'YYYY-MM-DD HH:mm:ss');
     };
 });
