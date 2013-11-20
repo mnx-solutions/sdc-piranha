4eb47d6de806c60ef312a3db78af7c9feaf12fab
Karl Düüna <karl.dyyna@gmail.com>
2013-11-20 13:48:07 +0200

    PIRANHA-878 - country codes included in phone number

---
+++ site/modules/account/static/js/directives/phone-number.js
@@ -0,0 +1,162 @@
+'use strict';
+
+(function (ng, app) {
+    app.directive('phoneNumber', [
+        'localization',
+        'notification',
+        '$q',
+        '$http',
+        function (localization, notification, $q, $http) {
+
+            return {
+                restrict: 'A',
+                replace: true,
+                scope: {
+                    currentStep: '@',
+                    number: '=',
+                    country: '@',
+                    isError: '&',
+                    elWidth: '@'
+                },
+                templateUrl: 'account/static/partials/phone-number.html',
+                link: function ($scope) {
+                    $scope.plainNumber = '';
+                    $scope.areaCode = '';
+
+                    $scope.$watch('plainNumber', function (plainNumber) {
+                        if(plainNumber) {
+                            $scope.number = $scope.areaCode + plainNumber;
+                        }
+                    });
+                    $scope.$watch('country', function (country) {
+                        if(typeof country === 'string' && $scope.countryCodes) {
+                            $scope.country = isoToObj(country);
+                            return;
+                        }
+
+                        if(country) {
+                            $scope.areaCode = '+' + country.areaCode;
+                            if($scope.plainNumber) {
+                                $scope.number = $scope.areaCode + $scope.plainNumber;
+                            }
+                        } else {
+                            $scope.areaCode = '';
+                        }
+                    });
+                    var numberWatcher = $scope.$watch('number', function (number) {
+                        if(!number) {
+                            return;
+                        }
+
+                        numberWatcher();
+
+                        var tempNumber = number.replace(/[^0-9\+]/g, '');
+                        if(tempNumber !== number) {
+                            $scope.number = tempNumber;
+                        }
+                        if(!$scope.countryCodes) {
+                            return;
+                        }
+                        matchCountry(number);
+                    });
+                    function matchCountry(number) {
+                        if(number.indexOf('+') !== 0) {
+                            var country = isoToObj($scope.country);
+                            $scope.plainNumber = number;
+                            $scope.areaCode = '+' + country.areaCode;
+                            $scope.number =  $scope.areaCode + number;
+                            $scope.country = country;
+                            return;
+                        }
+                        var pureNr = number.substr(1);
+                        var i = 1;
+                        var areaCode = pureNr.substr(0, i);
+                        var found = false;
+                        while(i < 4) {
+                            var countries = areaCodeMap[areaCode];
+                            if(countries) {
+                                $scope.areaCode = '+' + areaCode;
+                                $scope.plainNumber = pureNr.substr(i);
+                                if(countries.length === 1 || countries.indexOf($scope.country) === -1) {
+                                    $scope.country = areaCode == 1 ? $scope.country = isoToObj('USA') : countries[0];
+                                }
+                                found = true;
+                                break;
+                            }
+                            i++;
+                            areaCode = pureNr.substr(0, i);
+                        }
+                        if(!found) {
+                            console.error('Couldn\'t find country for nr:', number);
+                        }
+                    }
+
+                    var areaCodeMap = {};
+                    function createAreaCodeMap() {
+                        $scope.countryCodes.filter($scope.filterUndefinedAreas).forEach(function (country) {
+                            if(!areaCodeMap[country.areaCode]) {
+                                areaCodeMap[country.areaCode] = [country];
+                            } else {
+                                areaCodeMap[country.areaCode].push(country);
+                            }
+                        });
+                    }
+
+                    $scope.filterUndefinedAreas = function (country) {
+                        return country.areaCode;
+                    };
+
+                    $http.get('account/countryCodes').then(function(data) {
+                        $scope.countryCodes = data.data;
+                        createAreaCodeMap();
+                        if(typeof $scope.country === 'string') {
+                            $scope.country = isoToObj($scope.country);
+                        }
+                        if($scope.number) {
+                            matchCountry($scope.number);
+                        }
+                    });
+
+                    function isoToObj(iso) {
+                        if (!$scope.countryCodes){
+                            return;
+                        }
+
+                        var selected = null;
+                        var usa = null;
+
+                        $scope.countryCodes.some(function (el) {
+                            if (el.iso3 === 'USA') {
+                                usa = el;
+                            }
+
+                            if(el.iso3 === iso) {
+                                selected = el;
+                                return true;
+                            }
+                        });
+
+                        return selected || usa;
+                    };
+
+                    $scope.$watch('elWidth', function (width) {
+                        if(width) {
+                            $scope.selectStyle = {
+                                width: width + 'px'
+                            };
+                            $scope.inputStyle = {
+                                width: width - 75 + 'px'
+                            };
+                        } else {
+                            $scope.selectStyle = {
+                                width: '100%'
+                            };
+                            $scope.inputStyle = {
+                                width: '91%'
+                            };
+                        }
+                    });
+                }
+            };
+        }]);
+}(window.angular, window.JP.getModule('Account')));
--- site/modules/signup/static/js/controllers/phone.js
+++ site/modules/signup/static/js/controllers/phone.js
@@ -9,78 +9,29 @@
             localization.bind('signup', $scope);
 
             $scope.pin = null;
-
-            $scope.countryCodes = null;
-            $scope.country = null;
             $scope.phone = null;
-
-            $scope.selectedCountryCode = '1';
 
             $scope.callInProgress = false;
             $scope.lastCalledNumber = null;
-            $scope.fullPhoneNumber = null;
-
-            $scope.isoToObj = function(iso) {
-                if (!$scope.countryCodes) {
-                    return;
-                }
-                var selected = null;
-                var usa = null;
-                $scope.countryCodes.some(function (el) {
-                    if (el.iso3 === 'USA') {
-                        usa = el;
-                    }
-                    if (el.iso3 === iso) {
-                        selected = el;
-                        return true;
-                    }
-                });
-                return selected || usa;
-            };
-
-            Phone.getCountries(function (err, countries) {
-                if (err) {
-                    notification.replace('phone', { type: 'error' }, err);
-                    return;
-                }
-                $scope.countryCodes = countries;
-                $scope.country = $scope.isoToObj();
-                Account.getAccount(true).then(function (account) {
-                    if (!$scope.phone) {
-                        $scope.phone = account.phone || '';
-                    }
-                    if (!$scope.country || $scope.country.iso3 === 'USA') {
-                        $scope.country = $scope.isoToObj(account.country);
-                    }
-                });
+            $scope.numberChanged = false;
+            Account.getAccount(true).then(function (account) {
+                $scope.phone = account.phone || '';
             });
 
             $scope.$watch('phone', function (newVal) {
-                $scope.phone = newVal ? newVal.replace(new RegExp(/[^0-9#\*]/g), '') : newVal;
-                $scope.fullPhoneNumber = $scope.selectedCountryCode + $scope.phone;
-                if ($scope.fullPhoneNumber !== $scope.lastCalledNumber) {
-                    $scope.callInProgress = false;
-                }
-            });
-
-            $scope.$watch('country', function(newVal) {
-                $scope.selectedCountryCode = (newVal && newVal.areaCode) || '';
-                $scope.fullPhoneNumber = $scope.selectedCountryCode + $scope.phone;
-                if ($scope.fullPhoneNumber !== $scope.lastCalledNumber) {
-                    $scope.callInProgress = false;
-                }
+                $scope.numberChanged = newVal !== $scope.lastCalledNumber;
             });
 
             $scope.makeCall = function() {
-                if (!$scope.fullPhoneNumber || $scope.callInProgress) {
+                if (!$scope.phone || $scope.callInProgress || !$scope.numberChanged) {
                     return;
                 }
                 $scope.callInProgress = true;
-                $scope.lastCalledNumber = $scope.fullPhoneNumber;
-                Phone.makeCall($scope.fullPhoneNumber, function (err, data) {
+                $scope.lastCalledNumber = $scope.phone;
+                Phone.makeCall($scope.phone.replace(/[^0-9]/g,''), function (err, data) {
+                    $scope.callInProgress = false;
                     if (err) {
                         notification.replace('phone', { type: 'error' }, err);
-                        $scope.callInProgress = false;
                         if (data.navigate) {
                             $scope.updateStep();
                         }
@@ -99,8 +50,9 @@
                 if (!$scope.pin) {
                     return;
                 }
-                $scope.callInProgress = false;
+                $scope.callInProgress = true;
                 Phone.verify($scope.pin, function (err, data) {
+                    $scope.callInProgress = false;
                     if (err) {
                         notification.replace('phone', { type: 'error' }, err);
                         if (data.navigate) {
@@ -109,7 +61,6 @@
                         return;
                     }
                     Account.updateAccount({
-                        country: $scope.country.iso3,
                         phone: $scope.phone
                     }).then(function () {
                         notification.dismiss('phone');
