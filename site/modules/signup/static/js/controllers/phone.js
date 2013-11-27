a3387cfa2fe9543835604c3eb01216ee83d0610f
Henrik Peinar <henrikpeinar@gmail.com>
2013-11-27 12:24:36 +0200

    PIRANHA-929 Fixed instance detail view and signup/phone

--- site/modules/signup/static/js/controllers/phone.js
+++ site/modules/signup/static/js/controllers/phone.js
@@ -5,74 +5,78 @@
         'Signup.PhoneController',
         ['$scope', 'Account', 'localization', 'requestContext', 'notification', 'Phone', '$q',
             function ($scope, Account, localization, requestContext, notification, Phone, $q) {
-            requestContext.setUpRenderContext('signup.phone', $scope);
-            localization.bind('signup', $scope);
+                requestContext.setUpRenderContext('signup.phone', $scope);
+                localization.bind('signup', $scope);
 
-            $scope.pin = null;
-            $scope.phone = null;
+                $scope.pin = null;
 
-            $scope.callInProgress = false;
-            $scope.lastCalledNumber = null;
-            $scope.numberChanged = false;
-            Account.getAccount(true).then(function (account) {
-                $scope.phone = account.phone || '';
-            });
+                $scope.countryCodes = null;
+                $scope.country = null;
+                $scope.phone = null;
 
-            $scope.$watch('phone', function (newVal) {
-                $scope.numberChanged = newVal !== $scope.lastCalledNumber;
-            });
+                $scope.selectedCountryCode = '1';
 
-            $scope.makeCall = function() {
-                if (!$scope.phone || $scope.callInProgress || !$scope.numberChanged) {
-                    return;
-                }
-                $scope.callInProgress = true;
-                $scope.lastCalledNumber = $scope.phone;
-                Phone.makeCall($scope.phone.replace(/[^0-9]/g,''), function (err, data) {
-                    $scope.callInProgress = false;
-                    if (err) {
-                        notification.replace('phone', { type: 'error' }, err);
-                        $scope.phoneError = err;
-                        $scope.callInProgress = false;
-                        if (data.navigate) {
-                            $scope.updateStep();
-                        }
-                        return;
-                    }
-                    if (data.navigate) {
-                        $scope.phoneError = null;
-                        $scope.updateStep();
-                    } else {
-                        $scope.phoneError = null;
-                    }
+                $scope.callInProgress = false;
+                $scope.lastCalledNumber = null;
+
+                Account.getAccount(true).then(function (account) {
+                    $scope.phone = account.phone || '';
                 });
-            };
 
-            $scope.verifyPin = function () {
-                var expr = /^\d{4}$/g; //only 4 digits
-                $scope.pinError = null;
-                $scope.pinIsInvalid = !expr.test($scope.pin);
+                $scope.$watch('phone', function (newVal) {
+                    $scope.numberChanged = newVal !== $scope.lastCalledNumber;
+                });
 
-                if (!$scope.pin || $scope.pinIsInvalid) {
-                    return;
-                }
-                $scope.callInProgress = true;
-                Phone.verify($scope.pin, function (err, data) {
-                    $scope.callInProgress = false;
-                    if (err) {
-                        $scope.pinError = err;
-                        if (data.navigate) {
-                            $scope.updateStep();
-                        }
+                $scope.makeCall = function() {
+                    if (!$scope.phone || $scope.callInProgress || !$scope.numberChanged) {
                         return;
                     }
-                    Account.updateAccount({
-                        phone: $scope.phone
-                    }).then(function () {
-                        $scope.pinError = null;
-                        $scope.updateStep();
+                    $scope.callInProgress = true;
+                    $scope.lastCalledNumber = $scope.phone;
+                    Phone.makeCall($scope.phone, function (err, data) {
+                        if (err) {
+                            $scope.phoneError = err;
+                            $scope.callInProgress = false;
+                            if (data.navigate) {
+                                $scope.updateStep();
+                            }
+                            return;
+                        }
+                        if (data.navigate) {
+                            $scope.phoneError = null;
+                            $scope.updateStep();
+                        } else {
+                            $scope.phoneError = null;
+                        }
                     });
-                });
-            };
-        }]);
+                };
+
+                $scope.verifyPin = function () {
+                    var expr = /^\d{4}$/g; //only 4 digits
+                    $scope.pinError = null;
+                    $scope.pinIsInvalid = !expr.test($scope.pin);
+
+                    if (!$scope.pin || $scope.pinIsInvalid) {
+                        return;
+                    }
+                    $scope.callInProgress = false;
+                    Phone.verify($scope.pin, function (err, data) {
+                        if (err) {
+                            notification.replace('phone', { type: 'error' }, err);
+                            $scope.pinError = err;
+                            if (data.navigate) {
+                                $scope.updateStep();
+                            }
+                            return;
+                        }
+                        Account.updateAccount({
+                            phone: $scope.phone
+                        }).then(function () {
+                                notification.dismiss('phone');
+                                $scope.pinError = null;
+                                $scope.updateStep();
+                            });
+                    });
+                };
+            }]);
 }(window.JP.getModule('Signup')));
