c9512eab9eccf4637e099b88ccc4a879ca3a2cf4
Harri Siirak <harri.siirak@gmail.com>
2013-12-02 15:57:55 +0200

    PIRANHA-875 Clear MachineList call tracker

--- site/modules/dashboard/test/dashboard.scenario.js
+++ site/modules/dashboard/test/dashboard.scenario.js
@@ -40,7 +40,7 @@ describe('Dashboard', function () {
             return backend.track(ptor).call('MachineList').pending().then(function (isPending) {
                 return !isPending;
             });
-        });
+        }, 10000);
 
         var runningCount = backend.data('machines').reduce(function (prev, current, index) {
             var count = (typeof(prev) === 'number') ? prev : 0;
@@ -57,6 +57,10 @@ describe('Dashboard', function () {
 
         ptor.findElement(protractor.By.xpath('//body/div[2]/div/div/div/div/div/div[3]/div[1]/div/div[1]/div[1]')).then(function (elem) {
             expect(elem.getText()).toBe(runningCount + ' Running');
+        });
+
+        ptor.wait(function () {
+            return backend.track(ptor).call('MachineList').clear();
         });
     });
 
