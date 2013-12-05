383999fb2a05b3735c89ade1cb1a6c6735c16371
Dan Lukinykh <dan@silvertreesystems.com>
2013-12-05 20:28:54 +0200

    ELBAPI-21 return code back & handle 401st

--- site/modules/slb/startup.js
+++ site/modules/slb/startup.js
@@ -177,47 +177,51 @@ var slb = function execute(scope) {
     }
 
     function addSscKey(call, key, callback) {
-        call.cloud.deleteKey({name: 'ssc_public_key'}, function () {
-            call.cloud.createKey({name: 'ssc_public_key', key: key}, callback);
-        });
+        var slbmKey = {name: call.req.session.userName + '_ssc_public_key', key: key};
+        key = {name: 'ssc_public_key', key: key};
 
-//        var slbmKey = {name: call.req.session.userName + '_ssc_public_key', key: key};
-//        key = {name: 'ssc_public_key', key: key};
-//
-//        var pool = [
-//            function (cb) {
-//                call.cloud.getAccount('slb_meterings', function (err, account) {
-//                    if (err && err.statusCode === 404) {
-//                        call.req.log.error('User "slb_meterings" not found\nCan`t add public key');
-//                        return cb(null);
-//                    }
-//                    call.cloud.listKeys(account.id, function (err, keys) {
-//                        if (err) {return cb(err); }
-//                        var neededKey = Array.isArray(keys) && keys.filter(function (key) {
-//                            return key.name === slbmKey.name;
-//                        })[0];
-//                        if (!neededKey) {
-//                            call.cloud.createKey(account.id, slbmKey, function (err) {
-//                                console.log(arguments, slbmKey);
-//                                cb(err);
-//                            });
-//                        } else {
-//                            call.cloud.deleteKey(account.id, neededKey.id, function () {
-//                                call.cloud.createKey(account.id, slbmKey, function (err) {
-//                                    cb(err);
-//                                });
-//                            });
-//                        }
-//                    });
-//                });
-//            },
-//            function (cb) {
-//                call.cloud.deleteKey(key, function () {
-//                    call.cloud.createKey(key, cb);
-//                });
-//            }
-//        ];
-//        vasync.parallel({funcs: pool}, callback);
+        var pool = [
+            function (cb) {
+                var meteringsUser = 'slb_meterings';
+                call.cloud.getAccount(meteringsUser, function (err, account) {
+                    if (err) {
+                        var errorMessage = 'Cannot add public key to user "%s"';
+                        if (err.statusCode === 404) {
+                            errorMessage = errorMessage + ' because user not found';
+                        }
+                        else if (err.statusCode === 401) {
+                            errorMessage = errorMessage + '.  Access denied';
+                        }
+                        call.req.log.error(errorMessage, meteringsUser);
+                        return cb(null);
+                    }
+                    call.cloud.listKeys(account.id, function (err, keys) {
+                        if (err) {return cb(err); }
+                        var neededKey = Array.isArray(keys) && keys.filter(function (key) {
+                            return key.name === slbmKey.name;
+                        })[0];
+                        if (!neededKey) {
+                            call.cloud.createKey(account.id, slbmKey, function (err) {
+                                console.log(arguments, slbmKey);
+                                cb(err);
+                            });
+                        } else {
+                            call.cloud.deleteKey(account.id, neededKey.id, function () {
+                                call.cloud.createKey(account.id, slbmKey, function (err) {
+                                    cb(err);
+                                });
+                            });
+                        }
+                    });
+                });
+            },
+            function (cb) {
+                call.cloud.deleteKey(key, function () {
+                    call.cloud.createKey(key, cb);
+                });
+            }
+        ];
+        vasync.parallel({funcs: pool}, callback);
     }
 
     function removeSscConfig(data, callback) {
