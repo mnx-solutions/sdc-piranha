5a5b73dbdefc552ad6fc63716a8fc8384a51a3ac
endrik end <endrik.end@gmail.com>
2013-12-05 17:35:21 +0200

    PIRANHA-943 ssh key tests on account page

---
+++ site/modules/account/test/account-ssh.scenario.js
@@ -0,0 +1,67 @@
+// Load e2e configuration
+var config = require('easy-config').loadConfig({
+    folder: process.cwd() + '/test/e2e/config/'
+});
+var backend = require(process.cwd() + '/test/e2e/mocks/backend.js');
+var ptor = null;
+backend
+    .stub(ptor)
+    .call('getAccount', backend.data('account'))
+    .call('createKey', backend.data('create-key'))
+    .call('listKeys', backend.data('list-keys'));
+
+var publicKey = {
+    name: 'test public key',
+    key: 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSU' +
+         'GPl+nafzlHDTYW7hdI4yZ5ew18JH4JW9jbhUFrviQzM7xlELEVf4h9lFX5QVkbPppSwg0cda3' +
+         'Pbv7kOdJ/MTyBlWXFCR+HAo3FXRitBqxiX1nKhXpHAZsMciLq8V6RjsNAQwdsdMFvSlVK/7XA' +
+         't3FaoJoAsncM1Q9x5+3V0Ww68/eIFmb1zuUFljQJKprrX88XypNDvjYNby6vw/Pb0rwert/En' +
+         'mZ+AW4OZPnTPI89ZPmVMLuayrD2cE86Z/il8b+gw3r3+1nKatmIkjn2so1d01QraTlMqVSsbx' +
+         'NrRFi9wrf+M7Q== example@example.com'
+}
+
+describe('Account Billing info', function () {
+    var driver = null;
+    var sshKeys = 0;
+
+    beforeEach(function() {
+        ptor = protractor.getInstance();
+        driver = ptor.driver;
+        backend
+            .stub(ptor)
+            .flush();
+    });
+    describe('redirect user to account summary page', function() {
+        it('should send user to account summary page', function () {
+            ptor.get('/#!/account');
+        });
+    });
+
+    describe('Summary page', function () {
+        it('user should be able to import ssh keys', function () {
+            ptor.wait(function() {
+                return ptor.getCurrentUrl().then(function(url) {
+                    return url.match(/\/account/);
+                });
+            });
+            ptor.findElement(protractor.By.xpath('//button[@class="btn btn-joyent-blue pull-right ssh-add-button"]/span[@class="ng-scope"]')).click();
+
+            ptor.findElement(protractor.By.xpath('//div[@class="modal-body"]/div[1]/div/input')).sendKeys(publicKey.name);
+            ptor.findElement(protractor.By.xpath('//div[@class="modal-body"]/div[2]/div/textarea')).sendKeys(publicKey.key);
+
+            ptor.findElement(protractor.By.xpath('//div[@class="modal-footer"]/button[@class="btn ng-scope ng-binding btn-joyent-blue"]')).click();
+            var key = ptor.findElement(protractor.By.xpath('//div[@class="account span9"]/fieldset[@class="row-fluid"]/div[@class="span12"]/div[@class="row-fluid"]/div[@class="row-fluid"]/div[@class="item-list-container"]/div[@class="item row-fluid ng-scope"][1]/span[@class="pointer text-medium"]/span[@class="span12 title ng-binding"]'));
+            expect(key.getText()).toContain(publicKey.name);
+
+        });
+        it('user should be able to delete ssh keys', function () {
+
+            ptor.findElement(protractor.By.xpath('//div[@class="account span9"]/fieldset[@class="row-fluid"]/div[@class="span12"]/div[@class="row-fluid"]/div[@class="row-fluid"]/div[@class="item-list-container"]/div[@class="item row-fluid ng-scope"][1]/span[@class="pointer text-medium"]/span[@class="span12 title ng-binding"]')).click();
+            ptor.findElement(protractor.By.xpath('//div[@class="account span9"]/fieldset[@class="row-fluid"]/div[@class="span12"]/div[@class="row-fluid"]/div[@class="row-fluid"]/div[@class="item-list-container"]/div[@class="item row-fluid ng-scope"][1]/div[@class="toolbox span10"]/div[@class="pull-right span2"]/button[@class="btn btn-mini button btn-danger"]/span[@class="ng-scope"]')).click();
+            var key = ptor.findElement(protractor.By.xpath('//div[@class="account span9"]/fieldset[@class="row-fluid"]/div[@class="span12"]/div[@class="row-fluid"]/div[@class="row-fluid"]/div[@class="item-list-container"]/div[@class="item row-fluid ng-scope"][1]/span[@class="pointer text-medium"]/span[@class="span12 title ng-binding"]'));
+
+            expect(key.getText()).toContain('test2');
+        });
+
+    });
+});
