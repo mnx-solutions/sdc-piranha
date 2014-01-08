7b2a564de6ef24a6bfacc485ce349da7658edda8
Alexander Rysenko <rysenko@gmail.com>
2014-01-08 21:40:05 +0200

    no-JIRA Fix possibility to cancel signup

--- site/apps/signup/after-auth.js
+++ site/apps/signup/after-auth.js
@@ -1,9 +1,16 @@
 'use strict';
 
+var metadata = require('../../modules/account/lib/metadata');
+
 module.exports = function (req, res, next) {
-    if(!req.session.signupStep) {
+    if (!req.session.signupStep) {
         req.session.signupStep = 'start';
         req.log.info('User is now in step phone');
+        metadata.set(req.session.userId, metadata.SIGNUP_STEP, 'start', function (err) {
+            if (err) {
+                req.log.info('Failed to set start signup step in metadata');
+            }
+        });
         req.session.save();
     }
     res.locals.signupStep = req.session.signupStep;
