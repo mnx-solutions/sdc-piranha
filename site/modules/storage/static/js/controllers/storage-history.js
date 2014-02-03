f5ecd9ec4d9385d4f57daaeb993839cbd1cf767e
Alexander Rysenko <rysenko@gmail.com>
2014-02-03 11:45:00 +0200

    PIRANHA-1012 Add loading indicator to history page

--- site/modules/storage/static/js/controllers/storage-history.js
+++ site/modules/storage/static/js/controllers/storage-history.js
@@ -3,141 +3,142 @@
 (function (app) {
     app.controller(
         'Storage.HistoryController',
-        ['$scope', 'requestContext', 'localization', 'Storage','util','$dialog',
+        ['$scope', 'requestContext', 'localization', 'Storage', 'util', '$dialog',
                 function ($scope, requestContext, localization, Storage, util, $dialog) {
             localization.bind('storage', $scope);
             requestContext.setUpRenderContext('storage.history', $scope);
 
-                    $scope.jobs = Storage.listJobs();
+                $scope.loading = true;
 
-                    //temporarily function
-                    $scope.$watch('jobs', function (jobs) {
-                        if(jobs){
-                            jobs.forEach(function (job) {
-                                job.id = job.name;
-                            });
-                        }
+                $scope.jobs = Storage.listJobs();
 
-                    }, true);
-                    //end
-
-                    $scope.actionButton = function(){
-                        var flag = false;
-                        $scope.jobs.$$v.forEach(function (el) {
-                            if(el.checked){
-                                flag = true;
-                            }
+                //temporarily function
+                $scope.$watch('jobs', function (jobs) {
+                    if (jobs) {
+                        $scope.loading = false;
+                        jobs.forEach(function (job) {
+                            job.id = job.name;
                         });
-                        return flag;
-                    };
+                    }
 
-                    $scope.noCheckBoxChecked = function(){
-                        util.error(
-                            localization.translate(
-                                $scope,
-                                null,
-                                'Error'
-                            ),
-                            localization.translate(
-                                $scope,
-                                null,
-                                'No jobs selected for the action.'
-                            ),function(){
-                            }
-                        );
-                    };
+                }, true);
+                //end
 
-                    $scope.gridOrder = ['created'];
-                    $scope.gridProps = [
-                        {
-                            id: 'mtime',
-                            name: 'Date',
-                            sequence: 1,
-                            active: true
-                        },
-                        {
-                            id: 'name',
-                            name: 'Name',
-                            sequence: 2,
-                            active: true
-                        },
-                        {
-                            id: 'id',
-                            name: 'ID',
-                            sequence: 3,
-                            active: true
-                        },
-                        {
-                            id: 'tasks',
-                            name: '#Tasks',
-                            sequence: 4,
-                            active: true
-                        },
-                        {
-                            id: 'done',
-                            name: '#Done',
-                            sequence: 5,
-                            active: true
-                        },
-                        {
-                            id: 'error',
-                            name: '#Errors',
-                            sequence: 6,
-                            active: true
-                        },
-                        {
-                            id: 'outputs',
-                            name: '#Outputs',
-                            sequence: 7,
-                            active: true
+                $scope.actionButton = function(){
+                    var flag = false;
+                    $scope.jobs.$$v.forEach(function (el) {
+                        if(el.checked){
+                            flag = true;
                         }
-                    ];
-                    $scope.gridDetailProps = [];
+                    });
+                    return flag;
+                };
 
-                    $scope.gridActionButtons = [
-                        {
-                            label: 'Clone',
-                            disabled: function (object) {
-                                return false;
-                            },
-                            action: function (object) {
-                                if($scope.actionButton()) {
-                                    util.confirm(
-                                        localization.translate(
-                                            $scope,
-                                            null,
-                                            'Confirm: Clone this task'
-                                        ),
-                                        localization.translate(
-                                            $scope,
-                                            null,
-                                            'Clone this task'
-                                        ), function () {
-                                            $scope.jobs.$$v.forEach(function (el) {
-                                                if(el.checked){
-                                                    el.checked = false;
-                                                }
-                                            });
+                $scope.noCheckBoxChecked = function(){
+                    util.error(
+                        localization.translate(
+                            $scope,
+                            null,
+                            'Error'
+                        ),
+                        localization.translate(
+                            $scope,
+                            null,
+                            'No jobs selected for the action.'
+                        ),function(){
+                        }
+                    );
+                };
+
+                $scope.gridOrder = ['created'];
+                $scope.gridProps = [
+                    {
+                        id: 'mtime',
+                        name: 'Date',
+                        sequence: 1,
+                        active: true
+                    },
+                    {
+                        id: 'name',
+                        name: 'Name',
+                        sequence: 2,
+                        active: true
+                    },
+                    {
+                        id: 'id',
+                        name: 'ID',
+                        sequence: 3,
+                        active: true
+                    },
+                    {
+                        id: 'tasks',
+                        name: '#Tasks',
+                        sequence: 4,
+                        active: true
+                    },
+                    {
+                        id: 'done',
+                        name: '#Done',
+                        sequence: 5,
+                        active: true
+                    },
+                    {
+                        id: 'error',
+                        name: '#Errors',
+                        sequence: 6,
+                        active: true
+                    },
+                    {
+                        id: 'outputs',
+                        name: '#Outputs',
+                        sequence: 7,
+                        active: true
+                    }
+                ];
+                $scope.gridDetailProps = [];
+
+                $scope.gridActionButtons = [
+                    {
+                        label: 'Clone',
+                        disabled: function (object) {
+                            return false;
+                        },
+                        action: function (object) {
+                            if($scope.actionButton()) {
+                                util.confirm(
+                                    localization.translate(
+                                        $scope,
+                                        null,
+                                        'Confirm: Clone this task'
+                                    ),
+                                    localization.translate(
+                                        $scope,
+                                        null,
+                                        'Clone this task'
+                                    ), function () {
+                                        $scope.jobs.$$v.forEach(function (el) {
+                                            if(el.checked){
+                                                el.checked = false;
+                                            }
                                         });
-                                }else $scope.noCheckBoxChecked();
-                            },
-                            show: function (object) {
-                                return true;
-                            },
-                            tooltip: 'Clone this task',
-                            sequence: 1
-                        }
-                    ];
+                                    });
+                            }else $scope.noCheckBoxChecked();
+                        },
+                        show: function (object) {
+                            return true;
+                        },
+                        tooltip: 'Clone this task',
+                        sequence: 1
+                    }
+                ];
 
-                    $scope.exportFields = {
-                        ignore: []
-                    };
-                    $scope.columnsButton = false;
-                    $scope.actionsButton = true;
-                    $scope.instForm = true;
-                    $scope.enabledCheckboxes = true;
-
-
+                $scope.exportFields = {
+                    ignore: []
+                };
+                $scope.columnsButton = false;
+                $scope.actionsButton = true;
+                $scope.instForm = true;
+                $scope.enabledCheckboxes = true;
         }]
     );
 }(window.JP.getModule('Storage')));
