694abad7572dcfd0c259aa71ad7de5b1961e9b65
Alexander Rysenko <rysenko@gmail.com>
2014-02-18 13:24:02 +0200

    PIRANHA-1296 Key validation fix

--- site/modules/machine/static/js/directives/machine-collection-item.js
+++ site/modules/machine/static/js/directives/machine-collection-item.js
@@ -1,32 +1,20 @@
 'use strict';
 
 (function (app) {
-    app.directive('machineCollectionValue', [
+    app.directive('machineCollectionItem', [
         function () {
             return {
                 require: 'ngModel',
                 link: function ($scope, elm, attrs, ctrl) {
                     ctrl.$parsers.unshift(function (viewValue) {
-
                         // Not empty
                         if (!viewValue || viewValue.length === 0) {
                             // Both empty key and val are valid
                             ctrl.$setValidity('item', (!$scope.o.key || !$scope.o.val));
                             return viewValue;
                         }
-                        // No invalid characters
-                        if (/[^A-Z0-9\._\-]/i.test(viewValue)) {
-                            ctrl.$setValidity('item', false);
-                            return viewValue;
-                        }
-
-                        // Is unique key
-                        var notUnique = $scope.collection.some(function (tag, index) {
-                            if (tag.key === $scope.o.key && index !== $scope.key) {
-                                return true;
-                            }
-                        });
-                        if (notUnique) {
+                        // No invalid characters in key
+                        if (attrs.placeholder === 'Key' && /[^A-Z0-9\._\-]/i.test(viewValue)) {
                             ctrl.$setValidity('item', false);
                             return viewValue;
                         }
