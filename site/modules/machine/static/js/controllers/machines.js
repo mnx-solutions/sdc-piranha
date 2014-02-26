243b2c8fbfb622ef95a2cda5488b4f0927202523
Andrey Yatskov <andrey.yatskov@silvertreesystems.com>
2014-02-26 16:39:42 +0200

    PIRANHA-1293 Newer instances should be at the top add sorting by status

--- site/modules/machine/static/js/controllers/machines.js
+++ site/modules/machine/static/js/controllers/machines.js
@@ -167,7 +167,12 @@
                 }
             }
 
-            $scope.gridOrder = ['-created'];
+            var stateOrder = function (object) {
+                var statuses = {running: 3, stopped: 3, provisioning: 2, creating: 1};
+                return statuses[object.state];
+            };
+
+            $scope.gridOrder = [stateOrder, '-created'];
             $scope.gridProps = [
                 {
                     id: 'label',
@@ -214,7 +219,8 @@
                     id: 'state',
                     name: 'Status',
                     sequence: 7,
-                    active: true
+                    active: true,
+                    _getter: stateOrder
                 },
                 {
                     id: 'updated',
