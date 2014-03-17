609de1bdfd3cc3d14a8980c2645924037d34777b
Vladimir Bulyga <xx@ccxx.cc>
2014-03-17 13:11:44 +0200

    PIRANHA-1421 Create Account: Error appears on creating SSH key

--- site/modules/account/static/js/directives/account-ssh-create.js
+++ site/modules/account/static/js/directives/account-ssh-create.js
@@ -17,25 +17,25 @@
                 restrict: 'EA',
                 replace: true,
                 scope: true,
-                controller: function($scope, $element, $attrs, $transclude) {
+                controller: function ($scope, $element, $attrs, $transclude) {
                     localization.bind('account', $scope);
 
                     $rootScope.downloadLink = false;
                 },
                 link: function ($scope) {
                     /* SSH Key generation popup */
-                    $scope.generateSshKey = function() {
-                        var sshKeyModalCtrl = function($scope, dialog) {
+                    $scope.generateSshKey = function () {
+                        var sshKeyModalCtrl = function ($scope, dialog) {
                             $scope.keyName = '';
 
-                            $scope.close = function(res) {
-                                if(!res || !res.generate) {
+                            $scope.close = function (res) {
+                                if (!res || !res.generate) {
                                     $rootScope.loading = false;
                                 }
                                 dialog.close(res);
                             };
 
-                            $scope.generateKey = function() {
+                            $scope.generateKey = function () {
                                 $rootScope.loading = true;
                                 $scope.close({generate: true, keyName: $scope.keyName});
                             };
@@ -51,25 +51,25 @@
                         PopupDialog.custom(
                             opts,
                             function (data) {
-                                if(data && data.generate === true) {
+                                if (data && data.generate === true) {
                                     var createUrl = 'account/ssh/create/';
 
                                     $http.post(createUrl, {name: data.keyName})
-                                        .success(function(data) {
+                                        .success(function (data) {
 
-                                            var jobId = data.jobId;
-                                            if(data.success === true) {
+                                            var keyId = data.keyId;
+                                            if (data.success === true) {
 
-                                                var downloadLink = 'account/ssh/download/'+ jobId;
+                                                var downloadLink = 'account/ssh/download/' + keyId;
 
                                                 // as this is directive, we need to use rootScope here
                                                 $rootScope.pollingJob = true;
                                                 $rootScope.loading = false;
 
-                                                if($scope.updateKeys) {
-                                                    $scope.updateKeys(function() {
+                                                if ($scope.updateKeys) {
+                                                    $scope.updateKeys(function () {
                                                         // if we are in signup, show the download right away
-                                                        $scope.iframe = '<iframe src="'+ downloadLink +'"></iframe>';
+                                                        $scope.iframe = '<iframe src="' + downloadLink + '"></iframe>';
                                                         $rootScope.downloadLink = downloadLink;
 
                                                         PopupDialog.message(
@@ -88,68 +88,15 @@
                                                     });
                                                 } else {
                                                     // if we are in signup, show the download right away
-                                                    $scope.iframe = '<iframe src="'+ downloadLink +'"></iframe>';
+                                                    $scope.iframe = '<iframe src="' + downloadLink + '"></iframe>';
                                                     $rootScope.downloadLink = downloadLink;
+                                                    if ($scope.nextStep) {
+                                                        $http.get('/signup/account/signup/passSsh').success(function (data) {
+                                                            // marked ssh step as passed
+                                                            window.location.href = '/main/#!/account/ssh';
+                                                        });
+                                                    }
                                                 }
-
-                                                // start polling
-                                                var pollingJob = setInterval(function() {
-                                                    $http.get('account/ssh/job/'+ jobId).success(function(data) {
-                                                        if (data.success === true) {
-                                                            // user has downloaded the key
-                                                            if($scope.nextStep) {
-                                                                $http.get('/signup/account/signup/passSsh').success(function(data) {
-                                                                    // marked ssh step as passed
-                                                                    window.location.href = '/main/#!/account/ssh';
-                                                                });
-                                                            }
-                                                        }
-                                                        if (data.success === false) {
-                                                            // key download failed, show error
-                                                            PopupDialog.error(
-                                                                localization.translate(
-                                                                    $scope,
-                                                                    null,
-                                                                    'Error'
-                                                                ),
-                                                                localization.translate(
-                                                                    $scope,
-                                                                    null,
-                                                                    'SSH Key generation error.'
-                                                                ),
-                                                                function () {}
-                                                            );
-                                                        }
-                                                        if (data.success !== undefined) {
-                                                            $rootScope.loading = false;
-                                                            $rootScope.pollingJob = false;
-                                                            clearInterval(pollingJob);
-                                                            clearTimeout(pollingTimeout);
-                                                        }
-                                                    })
-                                                }, 1000);
-
-                                                var pollingTimeout = setTimeout(function() {
-                                                    clearInterval(pollingJob);
-                                                    clearTimeout(pollingTimeout);
-                                                    $rootScope.loading = false;
-                                                    $rootScope.pollingJob = false;
-                                                    PopupDialog.error(
-                                                        localization.translate(
-                                                            $scope,
-                                                            null,
-                                                            'Error'
-                                                        ),
-                                                        localization.translate(
-                                                            $scope,
-                                                            null,
-                                                            'SSH Key generation error.'
-                                                        ),
-                                                        function () {}
-                                                    );
-
-                                                }, 2*60*1000);
-
                                             } else {
                                                 // error
                                                 $rootScope.loading = false;
@@ -181,6 +128,6 @@
                     };
                 },
                 templateUrl: 'account/static/partials/account-ssh-create.html'
-        };
+            };
 }]);
 }(window.JP.getModule('Account')));
