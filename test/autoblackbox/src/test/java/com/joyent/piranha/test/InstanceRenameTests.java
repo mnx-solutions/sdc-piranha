7ff2127c624bf7068d8482cd16dbcfa825ff0604
Igor Trifonov <igor.trifonov@silvertreesystems.com>
2014-03-21 12:49:34 +0200

    PIRANHA-1465 refactor instance rename tests

--- test/autoblackbox/src/test/java/com/joyent/piranha/test/InstanceRenameTests.java
+++ test/autoblackbox/src/test/java/com/joyent/piranha/test/InstanceRenameTests.java
@@ -1,10 +1,12 @@
 package com.joyent.piranha.test;
 
 import com.joyent.piranha.Common;
-import com.joyent.piranha.pageobjects.InstancePage;
+import com.joyent.piranha.pageobject.InstanceDetails;
+import com.joyent.piranha.pageobject.Login;
+import com.joyent.piranha.pageobject.NavBarMenu;
+import com.joyent.piranha.pageobject.SideBarMenu;
 import com.joyent.piranha.util.TestWrapper;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,44 +17,50 @@ import static com.codeborne.selenide.Configuration.timeout;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
 
 public class InstanceRenameTests extends TestWrapper {
+
+    public static final String USER_NAME = System.getProperty("loginusr");
+    public static final String PASSWORD = System.getProperty("loginpw");
+    public static NavBarMenu navBarMenu;
+    public static SideBarMenu sideBarMenu;
+    public static InstanceDetails instanceDetails;
+    public static final String TEST_INSTANCE_NAME = Common.getTestInstanceName();
 
     @BeforeClass
     public static void openDashboard() {
         timeout = CHANGE_STATUS_TIMEOUT;
         baseUrl = BASE_URL;
-        open("/");
-        Common.login();
+        Login loginPage = open("/", Login.class);
+        loginPage.login(USER_NAME, PASSWORD);
+        navBarMenu = page(NavBarMenu.class);
+        sideBarMenu = page(SideBarMenu.class);
+        instanceDetails = sideBarMenu.clickCompute().getList().openInstanceDetails(TEST_INSTANCE_NAME);
     }
 
     @AfterClass
     public static void logout() {
-        open("/landing/forgetToken");
-    }
-
-    @Before
-    public void openImageList() {
-        Common.clickNavigationLink("Compute");
-        InstancePage.gotoInstanceDetails(Common.getTestInstanceName());
+        navBarMenu.clickAccountMenu().clickLogout();
     }
 
     @Test
     public void renameInstanceValidation() {
-        InstancePage.clickRenameInstanceIcon();
-        InstancePage.getInstanceNameField().sendKeys("!!!");
-        $(".loading-medium.wait-rename").waitWhile(visible, timeout);
+        instanceDetails.clickRenameInstanceIcon();
+        instanceDetails.getInstanceNameField().sendKeys("!!!");
+        instanceDetails.waitForMediumSpinnerDisappear();
         $(byText("Machine name can contain only letters, digits and signs like '.' and '-'.")).shouldBe(visible);
-        InstancePage.getInstanceNameField().clear();
-        InstancePage.getInstanceNameField().sendKeys("forFIrewallAutoTests");
+        instanceDetails.getInstanceNameField().clear();
+        instanceDetails.getInstanceNameField().sendKeys("forFIrewallAutoTests");
         $(byText("Machine name is already in use")).shouldBe(visible);
     }
 
     @Test
     public void renameInstance() {
         String instName = "NewInstName";
-        InstancePage.rename(instName);
-        InstancePage.getInstanceNameField().shouldHave(text(instName));
-        InstancePage.rename(Common.getTestInstanceName());
+        instanceDetails.rename(instName);
+        instanceDetails.getInstanceNameField().shouldHave(text(instName));
+        sideBarMenu.clickCompute().getList().openInstanceDetails(instName);
+        instanceDetails.rename(Common.getTestInstanceName());
     }
 }
