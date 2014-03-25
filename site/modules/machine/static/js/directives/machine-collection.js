df5c9e53da6f1049f54a09f8ea9b7f6b519c885d
Aider Abdurafeev <aider.abdurafeev@gmail.com>
2014-03-25 15:56:16 +0200

    PIRANHA-1341 Fix 'edit' state for duplicate item

--- site/modules/machine/static/js/directives/machine-collection.js
+++ site/modules/machine/static/js/directives/machine-collection.js
@@ -97,6 +97,7 @@
                                 }
                             ),
                             function () {
+                                obj.edit = obj.isNew;
                                 progressOn();
                                 persistCollection();
                             }
