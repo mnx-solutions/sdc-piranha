7b47426673c7064b599f3a2be9fe1a55d0a2af67
Andrey Yatskov <andrey.yatskov@silvertreesystems.com>
2014-04-18 18:08:10 +0300

    PIRANHA-1762 Usage graphs: js error: Invalid value for <rect> attribute

--- site/modules/utilization/static/js/directives/utilization-chart.js
+++ site/modules/utilization/static/js/directives/utilization-chart.js
@@ -91,7 +91,7 @@
                         ticksData.splice(0);
                         for (var day = 1; day <= daysMax; day++) {
                             var dayStr = data.year + '-' + pad(data.month) + '-' + pad(day);
-                            var zeroValue = day < currentMonthDaysMax ? Number.MIN_VALUE : 0;
+                            var zeroValue = day < currentMonthDaysMax ? 0.00000000000000001 : 0;
                             var amount = amountData[dayStr] || zeroValue;
                             ticksData.push(day);
                             graphData.daily.push({x: day, y: amount});
