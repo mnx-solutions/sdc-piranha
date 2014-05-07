180965f27b4a48ea4a2bc6a8ef2e94aaf508325a
Andrey Yatskov <andrey.yatskov@silvertreesystems.com>
2014-05-07 13:17:37 +0300

    PIRANHA-1862 Support Offerings that are not self-serve should be Marketo forms, not 'Call Sales'

--- site/modules/support/startup.js
+++ site/modules/support/startup.js
@@ -5,6 +5,7 @@ var pkg = require('./data/packages.json');
 module.exports = function execute(scope) {
 
     var server = scope.api('Server');
+    var Marketo = scope.api('Marketo');
 
     server.onCall('SupportListPackages', function (call) {
         call.done(null, pkg);
@@ -12,5 +13,18 @@ module.exports = function execute(scope) {
 
     server.onCall('SupportCallSales', function (call) {
         call.req.log.info('Request Call Sales ' + call.data);
+    });
+
+    server.onCall('SupportTracking', function (call) {
+        var data = call.data;
+        var marketoData = data.marketo;
+        Marketo.update(data.id, marketoData, function (err) {
+            if (err) {
+                call.log.error({error: err, data: marketoData}, 'Failed to update marketo account');
+                call.done(null);
+            }
+            call.log.debug(marketoData, 'Associate Marketo lead with SOAP API');
+            call.done(null, marketoData);
+        });
     });
 }
