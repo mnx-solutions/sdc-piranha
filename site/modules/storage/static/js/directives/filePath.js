06b995b7572f1a148d519fb27999aa7a48334e46
Alexey Shisterov <alexey.shisterov@silvertreesystems.com>
2014-05-20 18:59:46 +0400

    PIRANHA-1973 fix for incorrect paths like '//'

--- site/modules/storage/static/js/directives/filePath.js
+++ site/modules/storage/static/js/directives/filePath.js
@@ -6,8 +6,13 @@
             return {
                 require: 'ngModel',
                 link: function (scope, elm, attrs, ctrl) {
+                    scope.$watch('filePath', function (path) {
+                        if (path && path.length > 0) {
+                            ctrl.$setValidity('filePath', path[0] === '/');
+                        }
+                    });
                     ctrl.$parsers.unshift(function (viewValue) {
-                        ctrl.$setValidity('filePath', viewValue[0] === '/' && viewValue.length > 1);
+                        ctrl.$setValidity('filePath', viewValue[0] === '/' && viewValue.length > 1 && viewValue[1] !== '/');
                         return viewValue;
                     });
                 }
