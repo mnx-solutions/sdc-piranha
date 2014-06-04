62f2644c1aeb45312aa8e3e3f518da7fb094356c
Vladimir Bulyga <xx@ccxx.cc>
2014-06-04 14:56:47 +0400

    PIRANHA-2062 Can't create instance without name

--- site/modules/machine/static/js/directives/name.js
+++ site/modules/machine/static/js/directives/name.js
@@ -8,7 +8,7 @@
                 link: function (scope, elm, attrs, ctrl) {
                     ctrl.$parsers.unshift(function (viewValue) {
                         var type = scope.$eval(attrs.machineName);
-                        var isValid = viewValue.length > 0 && /^([a-z0-9\.\-]*)$/i.test(viewValue);
+                        var isValid = !viewValue.length || /^([a-z0-9\.\-]*)$/i.test(viewValue);
                         ctrl.$setValidity(type + 'Name', isValid);
                         return viewValue;
                     });
