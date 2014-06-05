695f708a688a84a5c675e62747362beaa072aa78
Andrey Yatskov <andrey.yatskov@silvertreesystems.com>
2014-06-05 15:39:21 +0400

    PIRANHA-1920 fix cross-browser text overflow formatting

--- site/modules/machine/static/js/directives/tooltip-fulltext.js
+++ site/modules/machine/static/js/directives/tooltip-fulltext.js
@@ -14,6 +14,26 @@
                                 .attr('data-toggle', 'tooltip')
                                 .attr('data-placement', 'top')
                                 .attr('data-html', 'true');
+
+                            var text = elm.innerHTML.trim();
+                            if (elm.offsetHeight < (elm.scrollHeight - 5) && text.split(' ').length > 1) {
+
+                                var t = element.clone().hide().css({
+                                    'position': 'absolute',
+                                    'width': 'auto',
+                                    'overflow': 'visible',
+                                    'max-width': 'inherit'
+                                });
+
+                                element.after(t);
+
+                                while (text.length > 0 && t.context.scrollHeight > elm.offsetHeight) {
+                                    text = text.substring(0, text.lastIndexOf(" "));
+                                    t.context.innerHTML = text + '...';
+                                }
+                                elm.innerHTML = text + '...';
+                            }
+
                         }
                     }, 0);
 
