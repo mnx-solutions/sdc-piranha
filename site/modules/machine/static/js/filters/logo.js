d6eb0f488f39e381f4ee4dce2a453fd0ba4b0864
Dmitry Shkilyov <dima@sfdev.com>
2014-06-09 15:25:51 +0400

    PIRANHA-2042 Apply code review

--- site/modules/machine/static/js/filters/logo.js
+++ site/modules/machine/static/js/filters/logo.js
@@ -63,12 +63,7 @@
                 "ghost": "ghost",
                 "postgresql": "postgresql",
                 "debian-7": "debian",
-                "scalearc-mssql-enterprise-2cpu-2gb": "scalearc",
-                "scalearc-mysql-enterprise-1cpu-1gb": "scalearc",
-                "scalearc-mysql-enterprise-2cpu-2gb": "scalearc",
-                "scalearc-mysql-enterprise-5cpu-10gb": "scalearc",
-                "scalearc-mysql-platinum-2cpu-2gb": "scalearc",
-                "scalearc-mysql-platinum-5cpu-32gb": "scalearc",
+                "scalearc": "scalearc",
                 "nginx": "nginx"
             };
 
