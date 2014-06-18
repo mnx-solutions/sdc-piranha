e599f5940fc71683e0172c147297240c3d24763a
YZaicev <zaicev@silvertreesystems.com>
2014-06-18 13:35:46 +0400

    PIRANHA-2106 Proposals for image tile tooltips

--- site/modules/machine/static/js/directives/tooltip-fulltext.js
+++ site/modules/machine/static/js/directives/tooltip-fulltext.js
@@ -5,7 +5,7 @@
         function () {
             return {
                 restrict: 'EA',
-                link: function (scope, element) {
+                link: function (scope, element, attrs) {
                     setTimeout(function () {
                         var elm = element[0];
 
@@ -13,21 +13,19 @@
                         var lineHeight = parseInt(element.css('line-height'), 10) / 2;
                         var scrollHeight = elm.scrollHeight - lineHeight;
                         if (elm.offsetHeight < scrollHeight || elm.offsetWidth < elm.scrollWidth) {
-                            element
-                                .addClass('tooltip-hover')
-                                .attr('data-toggle', 'tooltip')
-                                .attr('data-placement', 'top')
-                                .attr('data-html', 'true');
 
                             var text = elm.innerHTML.trim();
+                            var label = ' <span class="label label-default tooltip-hover" data-toggle="tooltip" data-placement="top" data-html="true" data-original-title=" ' + attrs.originalTitle + '">...</span>';
 
                             if (elm.offsetHeight < scrollHeight && text.split(' ').length > 1) {
                                 while (text.length > 0 && element.context.scrollHeight > elm.offsetHeight) {
                                     text = text.substring(0, text.lastIndexOf(" "));
-                                    element.context.innerHTML = text + '...';
+                                    element.context.innerHTML = text + label;
                                 }
+                            } else if (text.length > 100 && text.split(' ').length === 1) {
+                                text = text.substring(100, 0);
+                                element.context.innerHTML = text + label;
                             }
-
                         }
                     }, 0);
 
