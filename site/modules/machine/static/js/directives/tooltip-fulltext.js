8d17ce7cbb7a35b96eaa1fd3cd1b2a653afae475
YZaicev <zaicev@silvertreesystems.com>
2014-06-20 16:27:17 +0400

    PIRANHA-2151 Label '[...]' doesn't appear for image with long name or link in description

--- site/modules/machine/static/js/directives/tooltip-fulltext.js
+++ site/modules/machine/static/js/directives/tooltip-fulltext.js
@@ -12,23 +12,22 @@
                         // a tricky way to make this work correctly with different browsers
                         var lineHeight = parseInt(element.css('line-height'), 10) / 2;
                         var scrollHeight = elm.scrollHeight - lineHeight;
-                        if (elm.offsetHeight < scrollHeight || elm.offsetWidth < elm.scrollWidth) {
+                        var text = elm.innerHTML.trim();
+                        var label = ' <span class="label label-default tooltip-hover" data-toggle="tooltip" data-placement="top" data-html="true" data-original-title=" ' + text + '">...</span>';
 
-                            var text = elm.innerHTML.trim();
-                            var label = ' <span class="label label-default tooltip-hover" data-toggle="tooltip" data-placement="top" data-html="true" data-original-title=" ' + attrs.originalTitle + '">...</span>';
-
-                            if (elm.offsetHeight < scrollHeight && text.split(' ').length > 1) {
-                                while (text.length > 0 && element.context.scrollHeight > elm.offsetHeight) {
-                                    text = text.substring(0, text.lastIndexOf(" "));
-                                    element.context.innerHTML = text + label;
-                                }
-                            } else if (text.length > 100 && text.split(' ').length === 1) {
-                                text = text.substring(100, 0);
+                        if (elm.offsetHeight < scrollHeight && elm.offsetWidth >= elm.scrollWidth) {
+                            while (text.length > 0 && element.context.scrollHeight > elm.offsetHeight) {
+                                text = text.substring(0, text.length - 1);
+                                element.context.innerHTML = text + label;
+                            }
+                        }
+                        if (elm.offsetWidth < elm.scrollWidth && elm.offsetHeight >= scrollHeight) {
+                            while (text.length > 0 && element.context.scrollWidth > elm.offsetWidth) {
+                                text = text.substring(0, text.length - 1);
                                 element.context.innerHTML = text + label;
                             }
                         }
                     }, 0);
-
                 }
             };
         }]);
