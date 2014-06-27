4fab579db80add4f92974e0203bac85a69fa0b2a
Aider Abdurafeev <aider.abdurafeev@gmail.com>
2014-06-27 11:24:53 +0300

    PIRANHA-1969 User should stay on billing step while information is updated

--- site/modules/machine/static/js/controllers/provision.js
+++ site/modules/machine/static/js/controllers/provision.js
@@ -576,11 +576,14 @@
                 if ($scope.keys.length > 0) {
                     $scope.clickProvision();
                 } else {
-                    $scope.setCurrentStep(4);
-                    $scope.slideCarousel();
+                    var sshStepIndex = 4;
+                    var isSSHStep = nextStep(sshStepIndex);
                     $timeout(function () {
                         deleteProvisionStep(ACCOUNT_STEP_NAME);
-                        $scope.setCurrentStep(3);
+                        if (isSSHStep) {
+                            sshStepIndex = 3;
+                            $scope.setCurrentStep(sshStepIndex);
+                        }
                     }, 600);
                 }
 
@@ -616,10 +619,13 @@
             };
 
 
-
             var nextStep = function (step) {
-                $scope.setCurrentStep(step);
-                $scope.slideCarousel();
+                var isNextStep = step - $scope.currentSlidePageIndex === 1;
+                if (isNextStep) {
+                    $scope.setCurrentStep(step);
+                    $scope.slideCarousel();
+                }
+                return isNextStep;
             };
 
             $scope.clickProvision = function () {
