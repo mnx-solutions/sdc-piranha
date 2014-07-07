e5455ae5fa688287599a937898341bd73b9fd6b4
Dan Lukinykh <dan@silvertreesystems.com>
2014-07-07 18:13:32 +0400

    PIRANHA-2222 Add user to default users when adding role

--- site/modules/rbac/static/js/controllers/user.js
+++ site/modules/rbac/static/js/controllers/user.js
@@ -93,6 +93,7 @@
                             index = role.members.indexOf($scope.user.login);
                             if (index < 0) {
                                 role.members.push($scope.user.login);
+                                role.default_members.push($scope.user.login);
                                 updates.push($q.when(service.updateRole(role)));
                                 break;
                             }
