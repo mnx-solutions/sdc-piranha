799c706aa40c2f60b60a0abf45b1fee96eefbf54
YZaicev <zaicev@silvertreesystems.com>
2014-08-04 16:57:09 +0400

    PIRANHA-2465 IE10: Incorrect QR-code is displayed for the user

--- site/modules/tfa/static/js/config/routes.js
+++ site/modules/tfa/static/js/config/routes.js
@@ -1,8 +1,17 @@
 'use strict';
 
-window.JP.main.config(['$routeProvider', function ($routeProvider) {
+window.JP.main.config(['$routeProvider', '$httpProvider',
+    function ($routeProvider, $httpProvider) {
+        $httpProvider.defaults.cache = false;
+        if (!$httpProvider.defaults.headers.get) {
+            $httpProvider.defaults.headers.get = {};
+        }
+        // disable IE ajax request caching
+        $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
+
         $routeProvider
             .when('/tfa', {
                 action: 'tfa.index'
             });
-    }]);
+        }
+]);
