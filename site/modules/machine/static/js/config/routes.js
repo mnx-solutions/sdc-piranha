07b197ca03d56771b74a7f262b8e2f49b23e120a
Dmitry Shkilyov <dmitry.shkilyov@silvertreesystems.com>
2014-10-21 22:13:51 +0400

    PIRANHA-2545 Modify instance list page

--- site/modules/machine/static/js/config/routes.js
+++ site/modules/machine/static/js/config/routes.js
@@ -6,6 +6,9 @@ window.JP.main.config([
         routeProvider.when('/compute', {
             title: 'Compute',
             action: 'machine.index'
+        }).when('/compute/dockerHost', {
+            title: 'Compute',
+            action: 'machine.index'
         }).when('/compute/instance/:machineid', {
             title: 'Instance Details',
             action: 'machine.details',
