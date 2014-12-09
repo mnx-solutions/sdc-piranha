cf867562540202bc895a7250ef7046161b9aff15
Sergey <smaksimenko@silvertreesystems.com>
2014-12-09 17:30:47 +0300

    PIRANHA-2610 Validation error does not appear when creating instance with the name containing trailing dash or dot

--- site/modules/machine/static/js/directives/name.js
+++ site/modules/machine/static/js/directives/name.js
@@ -8,8 +8,19 @@
                 link: function (scope, elm, attrs, ctrl) {
                     ctrl.$parsers.unshift(function (viewValue) {
                         var type = scope.$eval(attrs.machineName);
-                        var isValid = viewValue.length === 0 || /^([a-z0-9\.\-]*)$/i.test(viewValue);
+                        var isValid = viewValue.length === 0 || /^([a-z0-9]+(([\.]{1}|[\-]+)[a-z0-9]+)*)$/i.test(viewValue);
+                        var isValidNameFirstLastChar = !(/^[\-\.]|[\-\.]$/.test(viewValue));
+                        var isNameContainDotDash = !(viewValue.indexOf('.-') !== -1 || viewValue.indexOf('-.') !== -1 || viewValue.indexOf('..') !== -1);
+                        if (!isNameContainDotDash) {
+                            isValid = true;
+                            isValidNameFirstLastChar = true;
+                        } else if (!isValidNameFirstLastChar) {
+                            isValid = true;
+                            isNameContainDotDash = true;
+                        }
                         ctrl.$setValidity(type + 'Name', isValid);
+                        ctrl.$setValidity('nameFirstLastChar', isValidNameFirstLastChar);
+                        ctrl.$setValidity('nameContainDotDash', isNameContainDotDash);
                         return viewValue;
                     });
                 }
