ecf874129939ade279c8392440dfe62bee09aa03
Dan Lukinykh <dan@silvertreesystems.com>
2015-01-09 11:39:19 +0300

    PIRANHA-3074 remove 'Any' from docker manta footer

--- site/modules/docker/static/js/directives/manta-disclaimer.js
+++ site/modules/docker/static/js/directives/manta-disclaimer.js
@@ -3,7 +3,7 @@
 (function (app) {
     app.directive('mantadisclaimer', function () {
         return {
-            template: '<b>Note:</b> Any Manta charges for storing Docker logs, private registries and images will apply. Please refer to <a href="https://www.joyent.com/products/manta/pricing" class="active-link" target="_blank">Joyent Manta Service Pricing</a>'
+            template: '<b>Note:</b> Manta charges for storing Docker logs, private registries and images will apply. Please refer to <a href="https://www.joyent.com/products/manta/pricing" class="active-link" target="_blank">Joyent Manta Service Pricing</a>'
         };
     });
 }(window.JP.getModule('docker')));
