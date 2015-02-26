5215de171ffe29727c21d1b691afa4467e4da30f
Alexander Rysenko <rysenko@gmail.com>
2015-02-26 18:31:42 +0300

    PIRANHA-3205 Portal crashes when uploading 'Linux kernel executable' file

--- site/static/js/services/http.js
+++ site/static/js/services/http.js
@@ -165,6 +165,9 @@
                 xhr.addEventListener('abort', function () {
                     cb(null, {status: 'success', id: chunkId, path: path});
                 }, false);
+                xhr.addEventListener('error', function () {
+                    cb(null, {status: 'error', message: 'Failed to read or upload file', id: chunkId, path: path});
+                }, false);
                 xhr.open('post', url, true);
                 xhr.send(data);
                 setTimeout(function () {
