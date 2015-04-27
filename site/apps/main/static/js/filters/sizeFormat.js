a6123d17191f53562d6c7073bfbad5251898d8fb
Sergey <smaksimenko@silvertreesystems.com>
2015-04-27 14:44:47 +0300

    PIRANHA-3416 Disk filter value dropdown should reflect the values in GB or TB on Select Package step

--- site/apps/main/static/js/filters/sizeFormat.js
+++ site/apps/main/static/js/filters/sizeFormat.js
@@ -5,6 +5,8 @@ window.JP.main.filter('sizeFormat', function () {
         size = bytes ? size : size * 1024 * 1024;
         var sizes = [' Bytes', ' KB', ' MB', ' GB', ' TB'];
         var i = parseInt(Math.floor(Math.log(size) / Math.log(1024)), 10);
-        return (Math.round(size / Math.pow(1024, i), 2) || 0) + (sizes[i] || sizes[0]);
+        var result = size / Math.pow(1024, i);
+        var places = result - parseInt(result, 10) ? 1 : 0;
+        return result.toFixed(places) + sizes[i];
     };
 });
