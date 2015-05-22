dde3b3f6878b7ebd306d84a8b87a4f79b06f73d2
YZaicev <zaicev@silvertreesystems.com>
2015-05-22 12:13:08 +0300

    PORTAL-1253 safari create instance url should work even if not already logged in

--- site/apps/main/before-auth.js
+++ site/apps/main/before-auth.js
@@ -4,7 +4,7 @@ module.exports = function mainBeforeAuthMiddleware(req, res, next) {
     // basically it's easy, if there's no token and the request is on root, we'll redirect to landing
     // if the request isn't to root it must be to a module, so we let it through this
     if (!req.session.token && req.path === '/') {
-        res.end('<script>location.href = "/?reload" + location.hash;</script>');
+        res.end('<script>location.replace(location.origin + "/?reload=/" + location.hash);</script>');
     } else {
         next();
     }
