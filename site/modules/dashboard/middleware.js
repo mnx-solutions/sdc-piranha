37aa4e2f3ecf8eb30fcdf7b67821dbdf752bdfb1
Sergey <smaksimenko@silvertreesystems.com>
2015-05-27 19:47:40 +0300

    PIRANHA-3513 Enable the campaignID to drive the content of the iframe on the main dashboard page

--- site/modules/dashboard/middleware.js
+++ site/modules/dashboard/middleware.js
@@ -9,6 +9,15 @@ module.exports = function dashboardMiddleware(req, res, next) {
         res.locals.jss = [];
     }
     res.locals.jss.push('window.JP.set("tritonDatacenter", "' + config.sdcDocker.datacenter + '")');
+    var campaigns = config.ns['campaigns'];
+    var marketing = config.marketing;
+    marketing.campaigns = [];
+    Object.keys(campaigns).forEach(function (campaignId) {
+        if (campaigns[campaignId].dashboardAd) {
+            marketing.campaigns.push(campaignId);
+        }
+    });
+    res.locals.jss.push('window.JP.set("marketing", ' + JSON.stringify(marketing) + ')');
 
     return next();
 };
